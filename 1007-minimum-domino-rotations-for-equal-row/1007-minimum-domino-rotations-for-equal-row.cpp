389cb9abd1d3ebd514d441b25b7fd921fdb9a04a
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-04-21 02:01:10 +0900

    Time: 271 ms (10.88%), Space: 121.1 MB (5.00%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> dp;
+    int N;
     
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int N = tops.size();
+        N = tops.size();
         
         dp.resize(N+1, -1);        
         vector<int> t = tops, b = bottoms;
@@ -11,19 +12,17 @@ public:
         
         dp.clear(); dp.resize(N+1, -1); 
         t = tops, b = bottoms;
-        int retBB = flip(0, b[0], t, b); // target at B[0], flip B
+        int retBB = flip(0, b[0], t, b); // target at B[0], flip if target == B[i]
         
         dp.clear(); dp.resize(N+1, -1); 
         t = tops, b = bottoms;
-        int retBT = flip(0, b[0], b, t); // target at B[0], flip T
+        int retBT = flip(0, b[0], b, t); // target at B[0], flip if target == T[i]
         
         dp.clear(); dp.resize(N+1, -1); 
         t = tops, b = bottoms;
-        int retTT = flip(0, t[0], b, t); // target at T[0], flip T
+        int retTT = flip(0, t[0], b, t); // target at T[0], flip if target == T[i]
         
-        int ret;
-        ret = min(retTB, retBB);
-        ret = min(ret, min(retBT, retTT));
+        int ret = min(min(retTB, retBB), min(retBT, retTT));
         if(ret == INT_MAX) ret = -1;
         return ret;
     }
@@ -31,7 +30,7 @@ public:
     int flip(int n, int target, vector<int>& t, vector<int>& b){
         int& ret = dp[n];
         if(ret != -1) return ret;
-        if(n == t.size()) return ret = 0;
+        if(n == N) return ret = 0;
      
         if(t[n] == target) {
             return ret = flip(n+1, target, t, b);
