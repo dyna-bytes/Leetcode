657c463669b27821867daf653ec9b337051d6c3a
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-04-20 16:28:08 +0900

    Time: 3 ms (57.60%), Space: 6.8 MB (34.41%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,17 +3,30 @@ public:
     vector<long long> dp;
     int combinationSum4(vector<int>& nums, int target) {
         if(target == 0) return 0;
-        dp.resize(target+1); // dp[n] := n을 만들 수 있는 경우의 수
+        dp.resize(target+1);
         
         dp[0] = 1;
-        for(int n = 1; n <= target; n++){
-            for(int num: nums){
-                if(n < num) continue;
-                if(dp[n] + dp[n - num] < INT_MAX)
-                    dp[n] += dp[n - num];
+        for(int i = 1; i <= target; i++){
+            for(int n : nums){
+                if(i >= n && dp[i] + dp[i-n] < INT_MAX)
+                    dp[i] += dp[i-n];
             }
         }
         return dp[target];
     }
     
+    /*
+    int f(vector<int>& nums, int target){
+        int& ret = dp[target];
+        if(ret) return ret;
+        if(target == 0) return ret =1;
+        
+        for(int n : nums){
+            if(target - n >= 0){
+                ret += (f(nums, target - n));
+            }
+        }
+        return ret;
+    }
+    */
 };
