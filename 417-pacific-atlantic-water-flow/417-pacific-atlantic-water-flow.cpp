79e0b9b3733a6e53622eff780a3d1ad4a488f14b
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-05-04 18:41:59 +0900

    Time: 76 ms (33.50%), Space: 32.6 MB (9.51%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -2,62 +2,57 @@ class Solution {
 public:
     int Y, X;
     
-    bool touchedPacific(int y, int x){
-        return (y < 0 || x < 0);
-    }
-    
-    bool touchedAtlantic(int y, int x){
-        return (y >= Y || x >= X);
-    }
-    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>> ret;
         Y = heights.size();
         X = heights[0].size();
-        int dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, -1, 1};
+      
+        set<pair<int, int>> pacific, atlantic;
         
-        for(int y = 0; y < Y; y++){
-            for(int x = 0; x < X; x++){
-                //vector<vector<bool>> visited(Y, vector<bool>(X));
-                int visited[200][200] = {0};
+        for(int y = 0; y < Y; y++)
+            BFS(y, 0, heights, pacific);
+        for(int x = 0; x < X; x++)
+            BFS(0, x, heights, pacific);
+        for(int y = 0; y < Y; y++)
+            BFS(y, X-1, heights, atlantic);
+        for(int x = 0; x < X; x++)
+            BFS(Y-1, x, heights, atlantic);
+        
+        set<pair<int, int>> intersect;
+        set_intersection(pacific.begin(), pacific.end(), atlantic.begin(), atlantic.end(),
+                 std::inserter(intersect, intersect.begin()));
+        
+        for(pair<int, int> pos : intersect){
+            ret.push_back({pos.first, pos.second});
+        }
+        return ret;
+    }
+    
+    bool inRange(int y, int x){
+        return (y >= 0 && y < Y && x >= 0 && x < X);
+    }
+    
+    void BFS(int sy, int sx, vector<vector<int>>& heights, set<pair<int, int>>& visited){
+        const int dy[4] = {-1, 1, 0, 0}, dx[4] = {0, 0, -1, 1};
+        
+        queue<pair<int, int>> q;
+        q.push({sy, sx});
+        visited.insert({sy, sx});
+        
+        while(!q.empty()){
+            pair<int, int> curr = q.front(); q.pop();
+            int y = curr.first, x = curr.second;
+            
+            for(int d = 0; d < 4; d++){
+                int ny = y + dy[d], nx = x + dx[d];
                 
-                int ocean[2] = {0};
-                queue<pair<int,int>> q;
-                q.push({y, x});
-                visited[y][x] = true;
+                if(!inRange(ny, nx)) continue;
+                if(visited.find({ny, nx}) != visited.end()) continue;
+                if(heights[y][x] > heights[ny][nx]) continue;
                 
-                while(!q.empty()){
-                    pair<int,int> curr = q.front(); 
-                    q.pop();
-                    int y = curr.first, x = curr.second;
-                    
-                    for(int d = 0; d < 4; d++){
-                        int ny = y + dy[d], nx = x + dx[d];
-                        
-                        if(ny < 0 || nx < 0){
-                            ocean[0] = true;
-                            continue;
-                        }
-                        if(ny >= Y || nx >= X){
-                            ocean[1] = true;
-                            continue;
-                        }
-                        
-                        if(visited[ny][nx]) continue;
-                        
-                        if(ocean[0] && ocean[1]) break;
-                                                
-                        if(heights[ny][nx] <= heights[y][x]){
-                            q.push({ny, nx});
-                            visited[ny][nx] = true;
-                        }
-                    }
-                }
-                
-                if(ocean[0] && ocean[1]) ret.push_back({y, x});
+                visited.insert({ny, nx});
+                q.push({ny, nx});
             }
         }
-        
-        return ret;
     }
 };
