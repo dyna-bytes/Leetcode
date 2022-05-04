82c6ef79d6980673e7072782b9e0fe2d6578659f
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-05-04 20:52:06 +0900

    Time: 980 ms (5.85%), Space: 102.5 MB (8.99%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -8,6 +8,7 @@ public:
     // 최소 중복 영역 개수 <=> 최대 중복되지 않는 영역 개수
     // 문제를 바꾸어서 생각해야 한다.
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return 0;  
         sort(intervals.begin(), intervals.end(), cmp);
         
         int count = 0;
