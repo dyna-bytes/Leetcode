6074ecb38a507780e2cc9d5ecf0f0982be1527f5
jihyuk1023 <59600347+jihyuk1023@users.noreply.github.com>
2022-05-22 23:22:36 +0900

    Time: 0 ms (100.00%), Space: 6 MB (91.99%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
-        return getMax(number, digit, 0);
-    }
-    
-    string getMax(string number, char target, int p){
-        
-        while(p < number.size() && number[p] != target) p++;
-        
-        if(p == number.size()) return "0";
-        
-        string currMax = number.substr(0, p) + number.substr(p+1);
-        if(number[p+1] > number[p]) return currMax;
-        
-        string nextMax = getMax(number, target, p+1);
-        if(cmp(currMax, nextMax)) return nextMax;
-        else return currMax;
-    }
-    
-    // if(n1 < n2) return true;
-    bool cmp(string& n1, string& n2){
-        for(int p = 0; p < n1.size() && p < n2.size(); p++){
-            if(n1[p] == n2[p]) continue;
+        int n = number.size();
+        int last_d;
+        for(int p = 0; p < n; p++){
+            if(p < n-1 && number[p] == digit && number[p+1] > number[p])
+                return number.substr(0, p) + number.substr(p+1);
             
-            if(n1[p] < n2[p]) return true;
+            if(number[p] == digit) last_d = p;
         }
-        return false;
+        
+        if(last_d == n-1) return number.substr(0, last_d);
+        return number.substr(0, last_d) + number.substr(last_d+1);
     }
 };
