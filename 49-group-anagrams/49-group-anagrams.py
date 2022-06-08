7b4379149907f7e2dcb0041286f4090c7c4f845a
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-06-09 02:59:49 +0900

    Time: 198 ms (17.50%), Space: 18.2 MB (46.63%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,24 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        primes = {"a": 2, "b": 3, "c": 5, "d": 7, "e": 11, "f": 13, "g": 17,
-        "h": 19, "i": 23, "j": 29, "k": 31, "l": 37, "m": 41, "n": 43, "o": 47,
-        "p": 53, "q": 59, "r": 61, "s": 67, "t": 71, "u": 73, "v": 79, "w": 83,
-        "x": 89, "y": 97, "z": 103}
-
-        products = []
-        for str in strs:
-            n = 1
-            for x in str:
-                n *= primes[x]
-            products.append(n)
-
-        hash = {}
-        ans = []
-        for i in range(len(strs)):
-            ans_idx = hash.get(products[i])
-            if ans_idx == None:
-                ans.append([strs[i]])
-                hash[products[i]] = len(ans) - 1
-            else:
-                ans[ans_idx].append(strs[i])
-        return ans
+        if len(strs) == 0: return [[]]
+        hash: Dict[Tuple, List[str]] = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            hash[key] = hash.get(key, []) + [s]
+        return hash.values()
