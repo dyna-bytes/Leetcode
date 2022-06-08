df1858192f6c699748e263fa1bf4744062c79ba8
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-06-08 20:21:15 +0900

    Time: 812 ms (5.61%), Space: 175.2 MB (28.87%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,82 +1,59 @@
-
-class LRUCache{
+class LRUCache {
 public:
-    struct Node{
-        int key, value;
-        Node* prev, * next;
-        Node(int k, int v){
-            key = k, value = v;
-            prev = next = nullptr;
-        }
-    };
+    typedef list<int> li; // {key}
+    typedef li::iterator pos;
+    typedef pair<int, pos> Node; // {value, position}
+    typedef unordered_map<int, Node*> unrd_map; // {key, {value, position}}
 
     int cap;
-    Node* head;// = new node(-1, -1);
-    Node* tail;// = new node(-1, -1);
-    unordered_map<int, Node*> map;
+    li used;
+    unrd_map map;
 
-    LRUCache(int capacity){
+    LRUCache(int capacity) {
         cap = capacity;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
     }
-
-    int get(int key){
+    
+    int get(int key) {
         auto ret = map.find(key);
         if(ret == map.end()) return -1;
 
         Node* node = ret->second;
+        int value = node->first;
         deleteNode(node);
-        insertBack(node);
-
-        // debugLRU();
-        return node->value;
+        insertBack(key, node);
+        return value;
     }
-
-    void put(int key, int value){
+    
+    void put(int key, int value) {
         if(get(key) != -1){
-            map.find(key)->second->value = value;
+            Node* node = map.find(key)->second;
+            node->first = value;
             return;
         }
         
-        Node* node = new Node(key, value);
-        insertBack(node);
+        Node* node = new Node();
+        node->first = value;
+        insertBack(key, node);
         map.insert({key, node});
 
-        if(map.size() > cap){            
-            Node* head_back = head->next;
-            map.erase(head_back->key);
-            deleteNode(head_back);
-            delete head_back;
+        if(map.size() > cap){
+            int head_key = used.front();
+            used.pop_front();
+            map.erase(head_key);
         }
-
-        // debugLRU();
     }
 private:
-    void debugLRU(){
-        for(Node* n = head; n != tail; n = n->next)
-            cout << "(" << n->key << ", " << n->value << ") -> ";
-        cout << "(" << tail->key << ", " << tail->value << ")" << endl;
-    }
-
-    void insertBack(Node* node){
-        Node* tail_front = tail->prev;
-        node->prev = tail_front;
-        node->next = tail;
-
-        tail_front->next = node;
-        tail->prev = node;
+    void insertBack(int key, Node* node){
+        used.push_back(key);
+        pos& node_it = node->second;
+        node_it = (--used.end());
     }
     void deleteNode(Node* node){
-        Node* node_front = node->prev;
-        Node* node_back = node->next;
-
-        node_front->next = node_back;
-        node_back->prev = node_front;
+        pos& node_it = node->second;
+        used.erase(node_it);
     }
 };
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
