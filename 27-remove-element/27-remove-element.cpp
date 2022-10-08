c12e2a5b14c5848d39b43c8c7e601af1b3b3845f
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-10-08 21:19:11 +0900

    Time: 12 ms (5.80%), Space: 8.9 MB (35.89%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -3,8 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
         if (nums.size() == 0) return 0;
         int N = nums.size();
-        int k = 0;
-        
+       
         int p = 0;
         for (int i = 0; i < N; i++) 
             if (nums[i] != val) 
