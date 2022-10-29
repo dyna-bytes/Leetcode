b0fb5f0dc91d402c5dfd92b28ae8e6e51581e57d
Jihyuk Park <59600347+jihyuk1023@users.noreply.github.com>
2022-10-29 13:48:19 +0900

    Time: 924 ms (12.13%), Space: 287.3 MB (7.25%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+typedef vector<int> vint;
+class Solution {
+public:
+    vector<vint> dp;
+    bool isPalindrome(int S, int E, string& str) {
+        int& ret = dp[S][E];
+        if (ret != -1) return ret;
+        if (S == E) return true;
+        if (S + 1 == E) return (str[S] == str[E]);
+
+        return ret = (str[S] == str[E]) && isPalindrome(S+1, E-1, str);
+    }
+    string longestPalindrome(string str) {
+        int N = str.size();
+        dp.resize(N, vint(N, -1));    
+        
+        string ret;
+        int max_len = 0;
+        for (int S = 0; S < N; S++)
+            for (int E = S; E < N; E++)
+                if (isPalindrome(S, E, str)) {
+                    int len = E - S + 1;
+                    if (max_len < len) {
+                        ret = str.substr(S, len);
+                        max_len = len;
+                    }
+                }
+        return ret;
+    }
+};
