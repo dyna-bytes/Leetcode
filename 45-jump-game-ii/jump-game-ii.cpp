5b17010e072f0851106046202a6a1748bd11cfbb
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-01-30 17:22:55 +0900

    Time: 0 ms (100.00%) | Memory: 21.6 MB (16.16%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    #define UNKNOWN -1
-    vector<int> memo;
-    int dp(int curr, const int N, const vector<int>& nums) {
-        if (curr >= N-1) return 0;
-        if (memo[curr] != UNKNOWN) return memo[curr];
-
-        int min_jump = 1e9;
-        for (int step = 1; step <= nums[curr]; step++) {
-            min_jump = min(min_jump, 1 + dp(curr + step, N, nums));
-        }
-        return memo[curr] = min_jump;
-    }
-
     int jump(vector<int>& nums) {
         int N = nums.size();
-        memo.resize(N, UNKNOWN);
-        return dp(0, N, nums);
+        if (N == 1) return 0;
+        vector<int> farthests(N, 0);
+
+        int farthest = 0;
+        for (int i = 0; i < N; i++) {
+            farthest = max(farthest, i + nums[i]);
+            farthests[i] = farthest;
+        }
+
+        int jumps = 1;
+        farthest = farthests[0];
+        for (int curr = 0; curr < N-1; curr++) {
+            if (curr == farthest) {
+                farthest = farthests[curr];
+                jumps++;
+            }
+        }
+        return jumps;
     }
 };
