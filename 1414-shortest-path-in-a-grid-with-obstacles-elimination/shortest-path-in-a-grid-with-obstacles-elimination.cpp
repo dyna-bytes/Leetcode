fd2c483509f66f1e3faf44319861438b3a1b0e44
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-02-25 23:45:13 +0900

    Time: 8 ms (87.28%) | Memory: 15.3 MB (80.43%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -14,7 +14,7 @@ public:
 
     int bfs(vector<vector<int>>& grid, int K) {
         queue<tuple<int, int, int, int>> Q; // {y, x, depth, step}
-        Q.push({0, 0, 0, 0});
+        Q.push({0, 0, 1, 0});
         visited[0][0] = 1;
         int steps = INT_MAX;
 
@@ -27,14 +27,12 @@ public:
             for (int d = 0; d < 4; d++) {
                 int ny = y + dy[d], nx = x + dx[d];
                 if (!inRange(ny, nx)) continue;
-                if (visited[ny][nx] && visited[ny][nx] - 1 <= depth)
-                    continue;
-                if (grid[ny][nx] && depth >= K)
-                    continue;
+                if (visited[ny][nx] && visited[ny][nx] <= depth) continue;
+                if (grid[ny][nx] && depth > K) continue;
                 
                 int next_depth = depth + (grid[ny][nx] == 1);
                 Q.push({ny, nx, next_depth, step + 1});
-                visited[ny][nx] = next_depth + 1;
+                visited[ny][nx] = next_depth;
             }
         }
 
