fb5905c216764b36c91ec78ef55dd07ebf11bfb5
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-03-02 01:12:17 +0900

    Time: 1490 ms (20.17%) | Memory: 459.7 MB (18.81%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,65 +1,59 @@
 class Solution {
 public:
+    typedef long long ll;
     const int mod = 1e9 + 7;
-    vector<vector<vector<int>>> memo;
+    vector<vector<vector<ll>>> memo;
 
-    int f(int N, int L, int A) {
+    int dp(int N, int L, int A) {
         // base case
-        if (N == 1) {
-            if (L == 2) { // use P or A
-                if (A == 1) // use P
-                    return 1;
-                else // use P or A
-                    return 2;
-            } else if (L == 1) {
-                if (A == 1) // use L or P
-                    return 2;
-                else // use L or A or P
-                    return 3;
-            } else if (L == 0) {
-                if (A == 1) // use L or P
-                    return 2;
-                else // use L or P or A
-                    return 3;
+        memo[1][1][0] = 1, memo[1][0][1] = 1, memo[1][0][0] = 1; // L, A, P
+
+        for (int n = 1; n < N; n++) {
+            // for L == 2
+            {
+                // for A == 1
+                memo[n+1][2][1] = memo[n][1][1]; // use L
+                // for A == 0
+                memo[n+1][2][0] = memo[n][1][0]; // use L
+            }
+
+            // for L == 1
+            {
+                // for A == 1
+                memo[n+1][1][1] = memo[n][0][1]; // use L
+                // for A == 0
+                memo[n+1][1][0] = memo[n][0][0]; // use L or A or P
+            }
+
+            // for L == 0
+            {
+                // for A == 1
+                ll& m = memo[n+1][0][1];
+                m = (m + memo[n][2][0]) % mod; // use A from 2 L
+                m = (m + memo[n][1][0]) % mod; // use A from 1 L
+                m = (m + memo[n][0][0]) % mod; // use A from 0 L
+                m = (m + memo[n][2][1]) % mod; // use P from 2 L
+                m = (m + memo[n][1][1]) % mod; // use P from 1 L
+                m = (m + memo[n][0][1]) % mod; // use P from 0 L
+
+                // for A == 0
+                ll& mm = memo[n+1][0][0];
+                mm = (mm + memo[n][2][0]) % mod; // use P from 2 L
+                mm = (mm + memo[n][1][0]) % mod; // use P from 1 L
+                mm = (mm + memo[n][0][0]) % mod; // use P from 0 L
             }
         }
 
-        int& ret = memo[N][L][A];
-        if (ret) return ret;
-        
-        if (L == 2) { // use P or A
-            if (A == 1) { // use P
-                ret = (ret + f(N-1, 0, A)) % mod; 
-            } else { // use P or A
-                ret = (ret + f(N-1, 0, 0)) % mod;
-                ret = (ret + f(N-1, 0, 1)) % mod;
-            }
-        } else if (L == 1) { // use L or A or P
-            if (A == 1) { // use L or P
-                ret = (ret + f(N-1, 2, A)) % mod;
-                ret = (ret + f(N-1, 0, A)) % mod;
-            } else { // use L or A or P
-                ret = (ret + f(N-1, 2, 0)) % mod;
-                ret = (ret + f(N-1, 0, 1)) % mod;
-                ret = (ret + f(N-1, 0, 0)) % mod;
-            }
-        } else if (L == 0) { // use L or A or P
-            if (A == 1) { // use L or P
-                ret = (ret + f(N-1, 1, A)) % mod;
-                ret = (ret + f(N-1, 0, A)) % mod;
-            } else { // use L or A or P
-                ret = (ret + f(N-1, 1, 0)) % mod;
-                ret = (ret + f(N-1, 0, 1)) % mod;
-                ret = (ret + f(N-1, 0, 0)) % mod;
-            }
-        }
-
+        ll ret = 0;
+        for (int l = 0; l < 3; l++)
+            for (int a = 0; a < 2; a++)
+                ret = (ret + memo[N][l][a]) % mod;
         return ret;
     }
 
     int checkRecord(int n) {
-        memo.resize(n+1, vector<vector<int>>(3, vector<int>(2, 0)));
+        memo.resize(n + 1, vector<vector<ll>>(3, vector<ll>(2, 0)));
 
-        return f(n, 0, 0);
+        return dp(n, 0, 0);
     }
 };
