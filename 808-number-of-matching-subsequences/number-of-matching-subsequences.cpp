ebf8a29848376bd55bd70d2082e531f90e0bdb42
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-03-02 23:22:13 +0900

    Time: 562 ms (13.94%) | Memory: 164.8 MB (24.31%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    unordered_map<string, int> matched;
+    vector<set<int>> dict;
+
     bool isSubsequence(string& s, string& word) {
-        if (matched[word]) return true;
-        
-        for (int i = 0, j = 0; i < s.size(); i++) {
-            if (s[i] == word[j]) j++;
-            
-            if (j == word.size()) {
-                matched[word] = true;
-                return true;
-            }
+        int prev_index = -1;
+
+        for (char c: word) {
+            auto it = dict[c].upper_bound(prev_index);
+            if (it == dict[c].end()) return false;
+            if (*it < prev_index) return false;
+            prev_index = *it;
         }
-        return false;
+        return true;
     } 
 
     int numMatchingSubseq(string s, vector<string>& words) {
+        dict.resize('z' + 1);
+        for (int i = 0; i < s.size(); i++)
+            dict[s[i]].insert(i);
+
         int match = 0;
         for (string& word: words) {
             if (isSubsequence(s, word))
