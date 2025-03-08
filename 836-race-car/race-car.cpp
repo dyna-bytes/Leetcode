204758a10a4748b099d44e5fe2497eb73748654c
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-03-08 12:51:40 +0900

    Time: 437 ms (31.71%) | Memory: 97.9 MB (26.46%) - LeetSync

--- 836-race-car/race-car.cpp
+++ 836-race-car/race-car.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
+    typedef long long ll;
     int racecar(int target) {
-#define MAX_N 20005
-typedef long long ll;
-        ll dp[MAX_N];
-        fill(dp, dp + MAX_N, INT_MAX);
+        map<pair<ll, ll>, bool> visited;
+        queue<pair<ll, ll>> q;
+        q.push({0, 1});
+        visited[{0, 1}] = true;
 
-        for (int t = 1; t <= target; t++) {
-            for (int n = 0; ; n++) {
-                int p = (1 << n) - 1;
-                if (p >= 2*t) break;
+        for (int step = 0; !q.empty(); step++) {
+            for (int level = q.size(); level > 0; level--) {
+                auto [pos, speed] = q.front();
+                q.pop();
 
-                if (p == t)
-                    dp[t] = n;
-                else if (p > t)
-                    dp[t] = min(dp[t], n + dp[p - t] + 1);
-                    // dp[p - q] = dp[p] + dp[q] + 1
-                else {
-                    for (int m = 0; m < n; m++) {
-                        int q = (1 << m) - 1;
-                        dp[t] = min(dp[t], n + m + 2 + dp[t - p + q]);
-                        // dp[p - q + r] = dp[p] + dp[q] + 1 + dp[r] + 1
-                    }
+                if (pos == target)
+                    return step;
+
+                pair<ll, ll> forward = {pos + speed, speed * 2};
+                if (!visited[forward] && 0 < pos + speed &&
+                    pos + speed < target * 2) {
+                    q.push(forward);
+                    visited[forward] = true;
+                }
+
+                pair<ll, ll> backward = {pos, speed > 0 ? -1 : 1};
+                if (!visited[backward] && 0 < pos && pos < target * 2) {
+                    q.push(backward);
+                    visited[backward] = true;
                 }
             }
         }
 
-        return dp[target];
+        return -1;
     }
 };
