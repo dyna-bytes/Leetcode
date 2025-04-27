53ec14a0040a755f058489bbd58eb2fa7a912bb5
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-04-27 17:13:27 +0900

    Time: 2 ms (2.99%) | Memory: 15.1 MB (66.69%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    int findPivot(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l < r && nums[l] > nums[r]) {
+            int m = (l + r) / 2;
+            if (nums[l] < nums[m])
+                l = m;
+            else
+                r = m;
+        }
+        return max(n - 1 - l, 0);
+    }
+    int searchTargetIndex(int s, int e, vector<int>& nums, int target) {
+        while (s + 1 < e) {
+            int m = (s + e) / 2;
+            if (target < nums[m])
+                e = m;
+            else
+                s = m;
+        }
+        return s;
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 1) {
+            if (target == nums[0]) return 0;
+            return -1;
+        }
+
+        int k = findPivot(nums);
+        cout << "k : " << k << endl;
+        int s, e;
+        if (nums[0] <= target && target <= nums[n - 1 - k]) {
+            s = 0, e = n - k;
+        } else if (nums[n - k] <= target && target <= nums[n - 1]) {
+            s = n - k, e = n;
+        } else
+            return -1;
+
+        int idx = searchTargetIndex(s, e, nums, target);
+        if (nums[idx] != target)
+            return -1;
+        return idx;
+    }
+};
