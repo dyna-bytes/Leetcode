4f8744d8de96d9c3fb3cb839b52c2baa39bbf436
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-05-06 14:38:55 +0900

    Time: 0 ms (100.00%) | Memory: 12.7 MB (93.59%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,13 +1,16 @@
 // #define DBG
 #ifdef DBG
-#define debug(x) std::cout << "[" << __func__ << "](" << __LINE__ << ") " \
-    << #x << " is " << x << endl
-#define debugVec(v) do { \
-    std::cout << "[" << __func__ << "](" << __LINE__ << ") " << #v; \
-    std::cout << " ["; \
-    for(int i = 0; i < ((v.size())-1); i++) std::cout << v[i] << "|"; \
-    std::cout << v[((v.size())-1)] << "]\n"; \
-} while(0)
+#define debug(x)                                                               \
+    std::cout << "[" << __func__ << "](" << __LINE__ << ") " << #x << " is "   \
+              << x << endl
+#define debugVec(v)                                                            \
+    do {                                                                       \
+        std::cout << "[" << __func__ << "](" << __LINE__ << ") " << #v;        \
+        std::cout << " [";                                                     \
+        for (int i = 0; i < ((v.size()) - 1); i++)                             \
+            std::cout << v[i] << "|";                                          \
+        std::cout << v[((v.size()) - 1)] << "]\n";                             \
+    } while (0)
 #else
 #define debug(x)
 #define debugVec(v)
@@ -15,51 +18,50 @@
 class Solution {
     class UnionFind {
         vector<int> root;
+
     public:
-        UnionFind(int N) {
-            root.resize(N, -1);
-        }
+        UnionFind(int N) { root.resize(N, -1); }
         int find(int n) {
             int& ret = root[n];
-            if (ret < 0) return n;
+            if (ret < 0)
+                return n;
             return ret = find(ret);
         }
         bool merge(int u, int v) {
             u = find(u);
             v = find(v);
-            if (u == v) return false;
+            if (u == v)
+                return false;
 
             if (root[u] >= root[v])
                 swap(u, v);
-            
+
             root[u] += root[v];
             root[v] = u;
             return true;
         }
     };
+
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         vector<int> ret;
         int N = edges.size();
-        UnionFind dsu(N+1);
+        UnionFind dsu(N + 1);
         vector<vector<int>> candidates;
 
-        for (auto& edge: edges) {
+        for (auto& edge : edges) {
             int u = edge[0], v = edge[1];
-            if (!dsu.merge(u, v)) {
-                debugVec(edge);
+            if (!dsu.merge(u, v)) 
                 candidates.push_back(edge);
-            }
         }
 
         int M = candidates.size();
-        for (int i = N-1; i >= 0; i--) {
+        for (int i = N - 1; i >= 0; i--) {
             auto& edge = edges[i];
-            debugVec(edge);
-            for (int j = M-1; j >= 0; j--) {
+            for (int j = M - 1; j >= 0; j--) {
                 auto& candi = candidates[j];
-                debugVec(candi);
-                if (edge == candi) return candi;
+                if (edge == candi)
+                    return candi;
             }
         }
 
