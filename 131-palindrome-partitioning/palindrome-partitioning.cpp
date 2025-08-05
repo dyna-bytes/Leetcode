118ce3357d0ddee6be6b7d88bb8fcb1116da79ec
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-08-05 22:12:48 +0900

    Time: 35 ms (76.16%) | Memory: 53.5 MB (85.29%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -6,22 +6,18 @@
 } while(0)
 
 class Solution {
-    #define FOUND 2
-    #define EMPTY 1
-    unordered_map<string, int> dp;
-    bool isPalindrome(string& str) {
-        if (str.size() == 0) return false;
-        int& ret = dp[str];
-        if (ret) return (ret == FOUND);
+    vector<vector<int>> dp;
+    bool isPalindrome(string& str, int s, int e) {
+        if (s >= e) return false;
+        int& ret = dp[s][e];
+        if (ret != -1) return ret;
 
-        for (int l = 0, r = str.size() - 1; l <= r; l++, r--)
+        for (int l = s, r = e - 1; l <= r; l++, r--)
             if (str[l] != str[r]){ 
-                ret = EMPTY;
-                return false;
+                return ret = 0;
             }
 
-        ret = FOUND;
-        return true;
+        return ret = true;
     }
 
     void f(string& str, int s, vector<string>& chosen, vector<vector<string>>& ret) {
@@ -31,8 +27,8 @@ class Solution {
         }
         
         for (int i = s + 1; i <= str.size(); i++) {
-            string substr = str.substr(s, i - s);
-            if (isPalindrome(substr)) {
+            if (isPalindrome(str, s, i)) {
+                string substr = str.substr(s, i - s);
                 chosen.push_back(substr);
                 f(str, i, chosen, ret);
                 chosen.pop_back();
@@ -40,15 +36,11 @@ class Solution {
         }
     }
 public:
-    bool check(string& str, int s, int e) {
-        if (s >= e) return false;
-        for (int l = s, r = e - 1; l <= r; l++, r--)
-            if (str[l] != str[r]) return false;
-        return true;
-    }
     vector<vector<string>> partition(string str) {
         vector<vector<string>> ret;
         vector<string> chosen;
+        int N = str.size();
+        dp.resize(N + 1, vector<int>(N + 1, -1));
         f(str, 0, chosen, ret);
         return ret;
     }
