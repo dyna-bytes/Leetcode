624946fb9b1bc0f51ab1559518d4ca3067db9d54
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-11 00:59:24 +0900

    Time: 169 ms (43.45%) | Memory: 172.3 MB (18.45%) - LeetSync

--- 1953-finding-mk-average/finding-mk-average.cpp
+++ 1953-finding-mk-average/finding-mk-average.cpp
@@ -1,107 +1,70 @@
 class MKAverage {
     int m, k;
-    long long mid_sum;
+    long long mid_sum = 0;
     queue<int> q;
     multiset<int> left, mid, right;
-
-    void deletingRebalance() {
-        if (left.size() < k) {
-            auto mid_bottom = mid.begin();
-            left.insert(*mid_bottom);
-
-            mid_sum -= *mid_bottom;
-            mid.erase(mid_bottom);
-        } else if (right.size() < k) {
-            auto mid_top = --mid.end();
-            right.insert(*mid_top);
-
-            mid_sum -= *mid_top;
-            mid.erase(mid_top);
-        }
+    bool remove_from_set(multiset<int>& ms, int x) {
+        auto it = ms.find(x);
+        if (it == ms.end()) return false;
+        ms.erase(it);
+        return true;
     }
-    void insertingRebalance(int num) {
-        auto left_top = --left.end();
-        auto right_bottom = right.begin();
-        if (num < *left_top) {
-            left.insert(num);
 
-            mid.insert(*left_top);
-            mid_sum += *left_top;
-            
-            left.erase(left_top);
-        } else if (num > *right_bottom) {
-            right.insert(num);
+    void rebalance_pull_left() {
+        while (left.size() < k) {
+            auto it = mid.begin();
+            left.insert(*it);
+            mid_sum -= *it;
+            mid.erase(it);
+        }
 
-            mid.insert(*right_bottom);
-            mid_sum += *right_bottom;
+        while (mid.size() < m - 2*k) {
+            auto it = right.begin();
+            mid.insert(*it);
+            mid_sum += *it;
+            right.erase(it);
+        }  
+    }
 
-            right.erase(right_bottom);
-        } else {
-            mid.insert(num);
-            mid_sum += num;
+    void rebalance_push_right() {
+        while (left.size() > k) {
+            auto it = --left.end();
+            mid.insert(*it);
+            mid_sum += *it;
+            left.erase(it);
+        }
+
+        while (mid.size() > m - 2*k) {
+            auto it = --mid.end();
+            right.insert(*it);
+            mid_sum -= *it;
+            mid.erase(it);
         }
     }
 public:
     MKAverage(int m, int k) {
         this->m = m;
         this->k = k;
-        mid_sum = 0;
     }
     
     void addElement(int num) {
-        auto removeNum = [&] (multiset<int>& group, int x) {
-            auto it = group.find(x);
-            if (it == group.end()) return false;
-            group.erase(it);
-            return true;
-        };
+        if (q.size() >= m) {
+            int x = q.front();
+            q.pop();
 
-        q.push(num);
-        if (q.size() < m) {
-            mid.insert(num);
-            mid_sum += num;
-        } else if (q.size() == m) {
-            mid.insert(num);
-            mid_sum += num;
-
-            // construct left and right.
-            // left size = k
-            // right size = k
-            // mid size = m - 2k
-            for (int i = 0; i < k; i++) {
-                auto mid_bottom = mid.begin();
-
-                left.insert(*mid_bottom);
-                mid_sum -= *mid_bottom;
-                mid.erase(mid_bottom);
-            }
-
-            for (int i = 0; i < k; i++) {
-                auto mid_top = --mid.end();
-
-                right.insert(*mid_top);
-                mid_sum -= *mid_top;
-                mid.erase(mid_top);
-            }
-        } else if (q.size() > m) {
-            // left size = k
-            // right size = k
-            // mid size = m - 2k + 1
-            insertingRebalance(num);
-
-            int x = q.front(); q.pop();
-            if (removeNum(mid, x)) // mid size = m - 2k, left size = right size = k
-                mid_sum -= x;
-            else if (removeNum(left, x)) ; // left size = k - 1, right size = k, mid size = m - 2k + 1
-            else removeNum(right, x) ; // right size = k - 1, left size = k, mid size = m - 2k + 1
-            
-            deletingRebalance();
+            if (remove_from_set(left, x)) ;
+            else if (remove_from_set(right, x)) ;
+            else if (remove_from_set(mid, x)) mid_sum -= x;
+            rebalance_pull_left();
         }
+        q.push(num);
+        left.insert(num);
+        rebalance_push_right();
     }
     
     int calculateMKAverage() {
         if (q.size() < m) return -1;
-        return mid_sum / (m - 2 * k);
+        return mid_sum / (m - 2*k);
     }
 };
 
