af590179c3c691019926bd255dfb1b7e27515ea3
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-11 16:32:35 +0900

    Time: 143 ms (60.36%) | Memory: 154.2 MB (69.37%) - LeetSync

--- 1953-finding-mk-average/finding-mk-average.cpp
+++ 1953-finding-mk-average/finding-mk-average.cpp
@@ -1,18 +1,8 @@
-#define debug(x) cout << #x << " is " << x << endl;
 class MKAverage {
     int m, k;
-    long long mid_sum;
+    long long mid_sum = 0;
     queue<int> q;
     multiset<int> lo, mid, hi;
-    void insert(int num) {
-        if (lo.size() < k || num <= *lo.rbegin())
-            lo.insert(num);
-        else if (hi.size() < k || num < *hi.begin()) {
-            mid.insert(num);
-            mid_sum += num;
-        } else
-            hi.insert(num);
-    }
 
     int remove(int x) {
         auto it = lo.find(x);
@@ -23,11 +13,11 @@ class MKAverage {
 
         it = mid.find(x);
         if (it != mid.end()) {
+            mid_sum -= *it;
             mid.erase(it);
-            mid_sum -= x;
             return 0;
         }
-
+        
         it = hi.find(x);
         if (it != hi.end()) {
             hi.erase(it);
@@ -36,6 +26,16 @@ class MKAverage {
         return -1;
     }
     void rebalance() {
+        while (lo.size() < k && mid.size()) {
+            lo.insert(*mid.begin());
+            mid_sum -= *mid.begin();
+            mid.erase(mid.begin());
+        }
+        while (hi.size() < k && mid.size()) {
+            hi.insert(*mid.rbegin());
+            mid_sum -= *mid.rbegin();
+            mid.erase(--mid.end());
+        }
         while (lo.size() > k) {
             mid.insert(*lo.rbegin());
             mid_sum += *lo.rbegin();
@@ -46,41 +46,59 @@ class MKAverage {
             mid_sum += *hi.begin();
             hi.erase(hi.begin());
         }
-        while (lo.size() < k && !mid.empty()) {
-            lo.insert(*mid.begin());
-            mid_sum -= *mid.begin();
+    }
+    void shuffle() {
+        while (lo.size() && mid.size() && *lo.rbegin() > *mid.begin()) {
+            int lo_top = *lo.rbegin();
+            int mid_btm = *mid.begin();
+
+            lo.erase(--lo.end());
             mid.erase(mid.begin());
+            mid_sum -= mid_btm;
+
+            lo.insert(mid_btm);
+            mid.insert(lo_top);
+            mid_sum += lo_top;
         }
-        while (hi.size() < k && !mid.empty()) {
-            hi.insert(*mid.rbegin());
-            mid_sum -= *mid.rbegin();
-            mid.erase(prev(mid.end()));
+
+        while (mid.size() && hi.size() && *mid.rbegin() > *hi.begin()) {
+            int mid_top = *mid.rbegin();
+            int hi_btm = *hi.begin();
+
+            mid.erase(--mid.end());
+            mid_sum -= mid_top;
+            hi.erase(hi.begin());
+
+            mid.insert(hi_btm);
+            mid_sum += hi_btm;
+            hi.insert(mid_top);
         }
     }
 public:
     MKAverage(int m, int k) {
         this->m = m;
         this->k = k;
-        mid_sum = 0;
     }
-
     
     void addElement(int num) {
         q.push(num);
-        insert(num);
+        mid.insert(num);
+        mid_sum += num;
 
         if (q.size() > m) {
             remove(q.front());
             q.pop();
         }
-        if (q.size() >= m) {
+
+        if (q.size() == m) {
             rebalance();
+            shuffle();
         }
     }
     
     int calculateMKAverage() {
         if (q.size() < m) return -1;
-        return mid_sum / (m - 2*k); 
+        return mid_sum / (m - 2 * k);
     }
 };
 
