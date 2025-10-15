c51d5e943974dee8bbc5cd0128a974d279af7d2f
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-15 18:50:10 +0900

    Time: 5 ms (59.45%) | Memory: 13.4 MB (37.46%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    unordered_map<char, int> um;
+public:
+    Solution () {
+        um['I'] = 1;
+        um['V'] = 5;
+        um['X'] = 10;
+        um['L'] = 50;
+        um['C'] = 100;
+        um['D'] = 500;
+        um['M'] = 1000;
+    }
+    
+    int romanToInt(string s) {
+        int num = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && um[s[i]] < um[s[i+1]]) {
+                num += (um[s[i+1]] - um[s[i]]);
+                i++;
+            } else {
+                num += um[s[i]];
+            }
+        }
+
+        return num;
+    }
+};
