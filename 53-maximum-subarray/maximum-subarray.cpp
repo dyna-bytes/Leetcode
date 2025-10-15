27d90981eece8bd81042192033fb8742b254eb5a
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-15 21:53:08 +0900

    Time: 1 ms (26.23%) | Memory: 71.8 MB (52.23%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,6 +2,7 @@
     dp[i] : largest sum of a subarray, that contains nums[i] and ends at i.
     dp[0] = nums[0]
     dp[i+1] = max(dp[i] + nums[i+1], nums[i+1])
+    각각의 최대 부분합은 이전 최대 부분합이 반영된 결과값
 */
 
 class Solution {
