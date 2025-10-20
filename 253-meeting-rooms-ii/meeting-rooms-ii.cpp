fbb432ce3d3176c73d23e1f9ee6d04639649d345
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-20 22:34:33 +0900

    Time: 15 ms (11.07%) | Memory: 20.4 MB (8.16%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        priority_queue<int, vector<int>, greater<int>> pq;
         sort(intervals.begin(), intervals.end());
 
-        for (auto& interval : intervals) {
+        int range = 0;
+        int prefix[(int)1e6 + 1];
+        memset(prefix, 0, sizeof(prefix));
+
+        for (auto& interval: intervals) {
             int s = interval[0];
             int e = interval[1];
 
-            if (pq.empty() || pq.top() > s) 
-                pq.push(e);
-            else {
-                pq.pop();
-                pq.push(e);
-            }
+            prefix[s]++;
+            prefix[e]--;
+
+            range = max(range, e);
         }
-        return pq.size();
+
+        int maxRooms = INT_MIN;
+        for (int i = 0; i < range; i++) {
+            prefix[i+1] = prefix[i] + prefix[i+1]; 
+            maxRooms = max(prefix[i+1], maxRooms);
+        }
+        return maxRooms;
     }
 };
