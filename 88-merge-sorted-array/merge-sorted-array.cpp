a398dee8cbb377199d33ffb10740bd14863958e6
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-23 21:30:56 +0900

    Time: 0 ms (100.00%) | Memory: 12.4 MB (39.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums = nums1;
-
+        if (m == 0) {
+            nums1 = nums2;
+            return;
+        }
+        if (n == 0)
+            return;
         int a, b, c;
-        for (a = 0, b = 0, c = 0; a < m && b < n; ) {
-            if (nums[a] < nums2[b])
-                nums1[c++] = nums[a++];
+
+        for (a = m - 1, b = n - 1, c = m + n - 1; a >= 0 && b >= 0;) {
+            if (nums1[a] > nums2[b])
+                nums1[c--] = nums1[a--];
             else
-                nums1[c++] = nums2[b++];
+                nums1[c--] = nums2[b--];
         }
 
-        if (a == m) {
-            for ( ; b < n; )
-                nums1[c++] = nums2[b++];
-        } else if (b == n) {
-            for ( ; a < m; )
-                nums1[c++] = nums[a++];
+        if (a <= 0) {
+            for (; b >= 0;)
+                nums1[c--] = nums2[b--];
+        } else if (b <= 0) {
+            for (; a >= 0;)
+                nums1[c--] = nums1[a--];
         }
     }
 };
