d425cec0c589d93ca1f7b925968a8e348478cb76
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-10-31 21:42:43 +0900

    Time: 15 ms (72.98%) | Memory: 157.9 MB (99.15%) - LeetSync

--- 4027-number-of-stable-subsequences/number-of-stable-subsequences.cpp
+++ 4027-number-of-stable-subsequences/number-of-stable-subsequences.cpp
@@ -1,44 +1,25 @@
-#define debug(x) cout << #x << " is " << x << endl;
-#define debugVect(v) do { \
-    cout << #v << " is |"; \
-    for (auto e:v) cout << e << "|"; \
-    cout << endl; \
-} while (0)
 class Solution {
     typedef long long ll;
     const ll MOD = 1e9 + 7;
-    vector<vector<vector<ll>>> dp; // dp[n][p][c]: [n, e) 까지 stable subseq 개수
-    int f(int n, int prev_parity, int count, vector<int>& nums) {
-        if (n == nums.size()) {
-            return 0;
-        }
-
-        // ll ret = 0;
-        ll& ret = dp[n][prev_parity][count];
-        if (ret != -1) return ret;
-
-        bool curr_parity = nums[n] & 1;
-        ret = f(n+1, prev_parity, count, nums); // skip current num
-
-        if (curr_parity == prev_parity && count == 2) {
-            // cannot choose current n
-        } else {
-            // choose current n
-            int new_count = (curr_parity == prev_parity) ? count + 1 : 1;
-            // expend with same parity (count + 1)
-            // or, expend with different parity (reset count = 1)
-            ret += (1 + f(n+1, curr_parity, new_count, nums));
-            // choosing current n -> +1 경우의 수
-            ret %= MOD;
-        }
-        
-        return ret;
-    }
 public:
     int countStableSubsequences(vector<int>& nums) {
-        dp.resize(nums.size() + 1, vector<vector<ll>>(2, vector<ll>(3, -1)));
-        return f(0, nums[0] & 1, 0, nums);
-        // initial_parity = (nums[0] & 1)
-        // 1 + f(n+1, nums[0]'s parity, count = 1, nums)
+        ll suffix_odd1 = 0, suffix_odd2 = 0;
+        ll suffix_even1 = 0, suffix_even2 = 0;
+
+        for (int n: nums) {
+            if ((n & 1) == 0) {
+                ll new_suffix_even1 = (suffix_even1 + suffix_odd1 + suffix_odd2 + 1) % MOD;
+                ll new_suffix_even2 = (suffix_even1 + suffix_even2) % MOD;
+                suffix_even1 = new_suffix_even1;
+                suffix_even2 = new_suffix_even2; 
+            } else {
+                ll new_suffix_odd1 = (suffix_odd1 + suffix_even1 + suffix_even2 + 1) % MOD;
+                ll new_suffix_odd2 = (suffix_odd1 + suffix_odd2) % MOD;
+                suffix_odd1 = new_suffix_odd1;
+                suffix_odd2 = new_suffix_odd2; 
+            }
+
+        }
+        return (suffix_even1 + suffix_even2 + suffix_odd1 + suffix_odd2) % MOD;
     }
 };
