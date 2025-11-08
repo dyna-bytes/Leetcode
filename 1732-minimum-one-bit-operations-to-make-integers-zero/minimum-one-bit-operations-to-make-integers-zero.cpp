b92afe1ce00f731635b346e59e8ca4b750af75d5
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-11-08 18:42:05 +0900

    Time: 42 ms (1.14%) | Memory: 8.2 MB (30.77%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,20 @@
+#define debug(x) cout << #x << " is " << x << endl;
+
+class Solution {
+    int f(int n, int bitsize) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        debug(n);
+        debug(bitsize);
+        int lsb = 1 << (bitsize - 1);
+        if (n == lsb) return 2*n - 1;
+
+        return f(lsb, bitsize) - f(n - lsb, floor(log2(n - lsb)) + 1);
+    }
+public:
+    int minimumOneBitOperations(int n) {
+        if (n == 0) return 0;
+        return f(n, floor(log2(n)) + 1);
+    }
+};
