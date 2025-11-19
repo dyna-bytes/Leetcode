164fd065be521678ad55c9e73064420607b76378
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-11-19 23:06:16 +0900

    Time: 3 ms (63.97%) | Memory: 8.3 MB (11.50%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    vector<int> dp;
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+
+        dp.resize(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 0; i < n - 1; i++)
+            dp[i+2] = dp[i] + dp[i+1];
+
+        return dp[n];
+    }  
+};
