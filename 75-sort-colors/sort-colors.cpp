4d6566ab58052489dfc8781a1f61b21a3c842b74
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-11-21 14:28:11 +0900

    Time: 3 ms (2.93%) | Memory: 11.7 MB (12.04%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = pq.top();
+            pq.pop();
+        }
+    }
+};
