a93d3ec4bbef0f6b67eab936837018c38c2414c6
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-11-25 23:55:25 +0900

    Time: 106 ms (70.00%) | Memory: 187.8 MB (69.60%) - LeetSync

--- 460-lfu-cache/lfu-cache.cpp
+++ 460-lfu-cache/lfu-cache.cpp
@@ -1,8 +1,8 @@
 // #define DEBUG
 #ifdef DEBUG
-#define debug(x) cout << "[" << __func__ << "]" << #x << " is " << x << endl;
+#define debug(x) cout << "[" << __func__ << "] " << #x << " is " << x << endl;
 #define debugSeq(s) do { \
-    cout << "[" << __func__ << "]" << #s << " is |"; \
+    cout << "[" << __func__ << "] " << #s << " is |"; \
     for (auto e : s) cout << "{ " << e.first << ", " << e.second << " }|"; \
     cout << endl; \
 } while (0)
@@ -11,154 +11,86 @@
 #define debugSeq(s)
 #endif
 
-typedef struct __node {
-    int key;
-    int val;
-    struct __node *prev;
-    struct __node *next;
-} node;
-
-class LRUCache {
-    node* head;
-    node* tail;
-    node dummy_head = {-1, -1, NULL, NULL};
-    node dummy_tail = {-1, -1, NULL, NULL};
+class LFUCache {
+    typedef tuple<int, int, int> tiii;
+    typedef tiii page; // { key, val, freq }
     int size;
     int cap;
-    unordered_map<int, node*> hash;
-
-    void initList() {
-        head = &dummy_head;
-        tail = &dummy_tail;
-        head->next = tail;
-        tail->prev = head;
-        size = 0;
-    }
-    void deleteNode(node* curr) {
-        node* prev = curr->prev;
-        node* next = curr->next;
-
-        prev->next = next;
-        next->prev = prev;
-    }
-    void insertLast(node* curr) {
-        node* last = tail->prev;
-
-        last->next = curr;
-        curr->prev = last;
-
-        curr->next = tail;
-        tail->prev = curr;
-    }
-public:
-    int getSize() { return size; }
-    unordered_map<int, node*>* getHash() { return &hash; }
-    LRUCache(int capacity=INT_MAX) {
-        cap = capacity;
-        initList();
-    }
-    void deleteNodeFromLRU(node* curr) {
-        hash.erase(curr->key);
-        deleteNode(curr);
-        size--;
-    }
-    node* popFront() {
-        assert(size != 0);
-        if (size == 0) return NULL;
-        node* front = head->next;
-        deleteNodeFromLRU(front);
-        return front;
-    }
-    node* getNodeWithoutCount(int key) {
-        if (hash.count(key) == 0) return NULL;
-        return hash[key];
-    }
-    
-    int get(int key) {
-        if (hash.count(key) == 0) return -1;
-
-        node* curr = hash[key];
-
-        deleteNode(curr);
-        insertLast(curr);
-        return curr->val;
-    }
-    
-    void put(int key, int value) {
-        if (get(key) != -1) {
-            node* curr = hash[key];
-            curr->val = value;
-            return;
-        }
-        
-        if (size >= cap) {
-            hash.erase(head->next->key);
-            deleteNode(head->next);
-            size--;
-        }
-
-        node* curr = new node(key, value, NULL, NULL);
-        hash[key] = curr;
-        insertLast(curr);
-        size++;
-    }
-};
-
-class LFUCache {
-    map<int, LRUCache*> freq; // {count : LRU list}
-    unordered_map<int, int> counts; // {key : count} 
-    int capacity;
-    int size = 0;
+    int minf;
+    unordered_map<int, list<page>> freq; // { freq: pages }
+    unordered_map<int, list<page>::iterator> lut; // { key, page* } 
 public:
     LFUCache(int capacity) {
-        this->capacity = capacity;
+        size = 0;
+        cap = capacity;
+        minf = 1;
     }
     
     int get(int key) {
-        if (counts.find(key) == counts.end()) return -1;
-        int cnt = counts[key];
-        node* curr = freq[cnt]->getNodeWithoutCount(key);
-        int val = curr->val;
+        debug(key);
 
-        freq[cnt]->deleteNodeFromLRU(curr);
-        if (freq[cnt]->getSize() == 0)
-            freq.erase(cnt);
+        if (lut.count(key) == 0) return -1;
+        list<page>::iterator p = lut[key];
+        auto [k, v, f] = *p;
+        
+        freq[f].erase(p);
+        if (freq[f].empty()) {
+            freq.erase(f);
+            if (f == minf)
+                minf++;
+        }
 
-        counts[key] = ++cnt;
-
-        if (freq.count(cnt) == 0)
-            freq[cnt] = new LRUCache();
-
-        freq[cnt]->put(key, val);
-        return val;
+        ++f;
+        freq[f].push_back({key, v, f});
+        lut[key] = prev(freq[f].end());
+        return v;
     }
     
     void put(int key, int value) {
-        if (get(key) != -1) {
-            node* curr = freq[counts[key]]->getNodeWithoutCount(key);
-            curr->val = value;
+        debug(key);
+        debug(value);
+
+        if (lut.count(key)) {
+            list<page>::iterator p = lut[key];
+            auto [k, v, f] = *p;
+            assert(k == key);
+
+            v = value;
+            freq[f].erase(p);
+            if (freq[f].empty()) {
+                freq.erase(f);
+                if (f == minf)
+                    minf++;
+            }
+
+            ++f;
+            freq[f].push_back({key, v, f});
+            lut[key] = prev(freq[f].end());
+
             return;
         }
 
-        if (size >= capacity) {
-            int LFU_count = freq.begin()->first;
-            auto LFU_list = freq.begin()->second;
-            node* LRU_node = LFU_list->popFront();
-            int LRU_key = LRU_node->key;
-            
-            if (LFU_list->getSize() == 0)
-                freq.erase(freq.begin());
-            counts.erase(LRU_key);
+        if (++size > cap) {
+            list<page>::iterator p = freq[minf].begin();
+            auto [k, v, f] = *p;
+            lut.erase(k);
 
-            size--;
+            debug(freq[minf].size());
+            freq[minf].pop_front();
+
+            if (freq[minf].empty()) {
+                freq.erase(minf);
+                if (f == minf)
+                    minf++;
+            }
+            
+            --size;
         }
 
-        int cnt = 1;
-        counts[key] = cnt;
-        if (freq.count(cnt) == 0)
-            freq[cnt] = new LRUCache();
-        freq[cnt]->put(key, value);
-        size++;
+        page p = { key, value, 1 };
+        freq[1].push_back(p);
+        lut[key] = prev(freq[1].end());
+        minf = 1;
     }
 };
 
