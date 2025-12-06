efbc4b37da7073298fa6bbfbc798a00fa312a9b3
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-07 01:41:42 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (46.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,11 +1,11 @@
 void moveZeroes(int* nums, int numsSize) {
     int n = numsSize;
-    int* ret = calloc(n, sizeof(int));
     int j = 0;
     for (int i = 0; i < n; i++) {
         if (nums[i] == 0) continue;
-        ret[j++] = nums[i];
+        nums[j++] = nums[i];
     }
-    memcpy(nums, ret, n * sizeof(int));
-    free(ret);
+
+    for (; j < n; j++)
+        nums[j] = 0;
 }
