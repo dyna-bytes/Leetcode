fdc714a54c55b4345992af74e20cbe884bbb7053
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-06 17:23:58 +0900

    Time: 20 ms (78.01%) | Memory: 77.6 MB (58.16%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        multiset<int, greater<int>> maxset;
-        multiset<int> minset;
-
         int n = nums.size();
-        int maxlen = 0;
-        maxset.insert(nums[0]);
-        minset.insert(nums[0]);
-        for (int s = 0, e = 0; s <= e && e < n; ) {
-            while (*maxset.begin() - *minset.begin() <= limit && e < n) {
-                maxlen = max(maxlen, e - s + 1);
-                
-                e++;
-                if (e >= n) return maxlen;
-                maxset.insert(nums[e]);
-                minset.insert(nums[e]);
-            }
+        deque<int> maxq; // descending
+        deque<int> minq; // ascending
+        
+        int s = 0;
+        int e = 0;
+        for (; e < n; e++) {
+            while (maxq.size() && nums[e] > maxq.back())
+                maxq.pop_back();
 
-            while (*maxset.begin() - *minset.begin() > limit && s < e) {
-                maxset.erase(maxset.find(nums[s]));
-                minset.erase(minset.find(nums[s]));
+            while (minq.size() && nums[e] < minq.back())
+                minq.pop_back();
+
+            maxq.push_back(nums[e]);
+            minq.push_back(nums[e]);
+
+            if (maxq.front() - minq.front() > limit) {
+                if (maxq.front() == nums[s]) maxq.pop_front();
+                if (minq.front() == nums[s]) minq.pop_front();
                 s++;
             }
         }
-        return maxlen;
+        return e - s;
     }
 };
