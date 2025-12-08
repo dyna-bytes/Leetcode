1e5bcd2d989d4aaceedae817d0f7a871b3cd39ac
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-08 22:06:24 +0900

    Time: 249 ms (10.00%) | Memory: 7.7 MB (72.50%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.c
@@ -0,0 +1,8 @@
+int countTriples(int n) {
+    int ret = 0;
+    for (int a = 1; a <= n; a++) 
+        for (int b = 1; b <= n; b++)
+            for (int c = 1; c <= n; c++)
+                if (a*a + b*b == c*c) ret++;
+    return ret;
+}
