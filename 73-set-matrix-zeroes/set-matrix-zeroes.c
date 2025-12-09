229442442b407d7104fa40a502fb0446665ae599
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-09 22:40:14 +0900

    Time: 0 ms (100.00%) | Memory: 16.3 MB (94.71%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.c
+++ 73-set-matrix-zeroes/set-matrix-zeroes.c
@@ -2,20 +2,28 @@ void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
     int Y = matrixSize;
     int X = *matrixColSize;
 
-    int* ycheck = calloc(Y, sizeof(int));
-    int* xcheck = calloc(X, sizeof(int));
-    for (int y = 0; y < Y; y++)
+    bool is_col = false;
+
+    for (int y = 0; y < Y; y++) {
+        if (matrix[y][0] == 0)
+            is_col = true;
+        for (int x = 1; x < X; x++)
+            if (matrix[y][x] == 0)  {
+                matrix[y][0] = 0;
+                matrix[0][x] = 0;
+            }
+    }
+
+    for (int y = 1; y < Y; y++) 
+        for (int x = 1; x < X; x++)
+            if (matrix[y][0] == 0 || matrix[0][x] == 0)
+                matrix[y][x] = 0;
+
+    if (matrix[0][0] == 0)
         for (int x = 0; x < X; x++)
-            if (matrix[y][x] == 0)
-                ycheck[y] = true, xcheck[x] = true;
-
-    for (int y = 0; y < Y; y++)
-        if (ycheck[y])
-            for (int x = 0; x < X; x++)
-                matrix[y][x] = 0;
-
-    for (int x = 0; x < X; x++)
-        if (xcheck[x])
-            for (int y = 0; y < Y; y++)
-                matrix[y][x] = 0;
+            matrix[0][x] = 0;
+        
+    if (is_col)
+        for (int y = 0; y < Y; y++)
+            matrix[y][0] = 0;
 }
