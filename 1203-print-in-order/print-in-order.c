3e61ebe48df9147e1c15aface78c0fbfb49aa69f
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-20 17:42:04 +0900

    Time: 0 ms (100.00%) | Memory: 9.1 MB (16.92%) - LeetSync

--- 1203-print-in-order/print-in-order.c
+++ 1203-print-in-order/print-in-order.c
@@ -3,9 +3,8 @@
 
 typedef struct {
     // User defined data may be declared here.
-    int cnt;
-    pthread_mutex_t m;
-    pthread_cond_t c;
+    sem_t first_done;
+    sem_t second_done;
 } Foo;
 
 // Function Declaration, do not remove
@@ -17,58 +16,32 @@ Foo* fooCreate() {
     Foo* obj = (Foo*) malloc(sizeof(Foo));
     
     // Initialize user defined data here.
-    obj->cnt = 0;
-    pthread_mutex_init(&obj->m, NULL);
-    pthread_cond_init(&obj->c, NULL);
-
+    sem_init(&obj->first_done, 0, 0);
+    sem_init(&obj->second_done, 0, 0);
     return obj;
 }
 
 void first(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 0)
-        pthread_cond_wait(&obj->c, &obj->m);
-    pthread_mutex_unlock(&obj->m);
-
+    
     // printFirst() outputs "first". Do not change or remove this line.
     printFirst();
-
-    pthread_mutex_lock(&obj->m);
-    obj->cnt = 1;
-    pthread_cond_broadcast(&obj->c);
-    pthread_mutex_unlock(&obj->m);
+    sem_post(&obj->first_done);
 }
 
 void second(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 1) 
-        pthread_cond_wait(&obj->c, &obj->m);
-    pthread_mutex_unlock(&obj->m);
-    
+    sem_wait(&obj->first_done);
     // printSecond() outputs "second". Do not change or remove this line.
     printSecond();
-
-    pthread_mutex_lock(&obj->m);
-    obj->cnt = 2;
-    pthread_cond_broadcast(&obj->c);
-    pthread_mutex_unlock(&obj->m);
+    sem_post(&obj->second_done);
 }
 
 void third(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 2)
-        pthread_cond_wait(&obj->c, &obj->m);
-    pthread_mutex_unlock(&obj->m);
-    
+    sem_wait(&obj->second_done);
     // printThird() outputs "third". Do not change or remove this line.
     printThird();
 }
 
 void fooFree(Foo* obj) {
     // User defined data may be cleaned up here.
-    pthread_mutex_destroy(&obj->m);   
-    pthread_cond_destroy(&obj->c);   
-
-    if (obj)
-        free(obj);
+    
 }
