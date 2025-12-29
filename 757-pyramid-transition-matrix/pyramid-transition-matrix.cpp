8405dd962f8fb0bb1c94df2014075e7283c3ea07
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2025-12-29 23:15:03 +0900

    Time: 2453 ms (5.41%) | Memory: 18.4 MB (29.73%) - LeetSync

---
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    unordered_map<string, bool> dp;
+    unordered_map<string, vector<char>> allowedset;
+    bool f(int i, string bottom, string top) {
+        if (bottom.size() <= 1) return true;
+
+        if (i + 1 == bottom.size())
+            return f(0, top, "");
+
+        if (i == 0 && dp.count(bottom))
+            return dp[bottom];
+
+        string curr = bottom.substr(i, 2);
+        bool ret = false;
+        for (char allowed: allowedset[curr])
+            ret += f(i + 1, bottom, top + allowed);
+        return dp[bottom] = ret;
+    }
+public:
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
+        for (auto& str: allowed) 
+            allowedset[str.substr(0, 2)].push_back(str[2]);
+        return f(0, bottom, "");
+    }
+};
