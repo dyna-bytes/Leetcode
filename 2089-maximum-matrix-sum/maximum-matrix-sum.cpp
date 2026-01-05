f4d38bf8ab7ac1aa42b2db3025be8ee195cd9270
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-01-05 21:14:23 +0900

    Time: 22 ms (5.19%) | Memory: 55.3 MB (5.66%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -4,18 +4,18 @@ public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         vector<ll> nums;
         int negs = 0;
+        ll min_v = INT_MAX;
         for (auto& row: matrix)
             for (int v: row) {
                 if (v <= 0) negs++;
                 nums.push_back(abs(v));
+                min_v = min(min_v, (ll)abs(v));
             }
 
         ll sum = accumulate(nums.begin(), nums.end(), 0LL);
         if (negs % 2 == 0)
             return sum;
 
-        sort(nums.begin(), nums.end());
-        ll min_v = nums[0];
         return sum - 2*min_v;
     }
 };
