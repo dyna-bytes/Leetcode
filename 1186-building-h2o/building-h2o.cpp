657a8771c1c9e90ca7413c744e199a8d4aa39cd9
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-01-11 01:03:12 +0900

    Time: 4 ms (64.48%) | Memory: 12.2 MB (18.73%) - LeetSync

--- 1186-building-h2o/building-h2o.cpp
+++ 1186-building-h2o/building-h2o.cpp
@@ -3,62 +3,70 @@
 class H2O {
     int H;
     int O;
-    int released;
+    int releaseH;
+    int releaseO;
     pthread_mutex_t m;
-    pthread_cond_t cv;
+    pthread_cond_t wakeupH, wakeupO;
 public:
     H2O() {
         H = 0, O = 0;
-        released = 0;
+        releaseH = 0, releaseO = 0;
         pthread_mutex_init(&m, NULL);
-        pthread_cond_init(&cv, NULL);
+        pthread_cond_init(&wakeupH, NULL);
+        pthread_cond_init(&wakeupO, NULL);
     }
 
     void hydrogen(function<void()> releaseHydrogen) {
         pthread_mutex_lock(&m);
         while (H >= 2)
-            pthread_cond_wait(&cv, &m);
+            pthread_cond_wait(&wakeupH, &m);        
         H++;
 
-        if (H == 2 && O == 1)
-            pthread_cond_broadcast(&cv);
-        else
-            while (H < 2 || O < 1)
-                pthread_cond_wait(&cv, &m);
+        if (H == 2 && O == 1) {
+            pthread_cond_broadcast(&wakeupH);
+            pthread_cond_broadcast(&wakeupO);
+        } else {
+            while (H < 2 || O < 1) 
+                pthread_cond_wait(&wakeupH, &m);
+        }
 
         releaseHydrogen();
-        released++;
+        releaseH++;
 
-        if (released == 3) {
-            H = 0;
-            O = 0;
-            released = 0;
-            pthread_cond_broadcast(&cv);
+        if (releaseH == 2 && releaseO == 1) {
+            H = 0, O = 0;
+            releaseH = 0, releaseO = 0;
+            pthread_cond_broadcast(&wakeupH);
+            pthread_cond_broadcast(&wakeupO);
         }
+        
         pthread_mutex_unlock(&m);
     }
 
     void oxygen(function<void()> releaseOxygen) {
         pthread_mutex_lock(&m);
         while (O >= 1)
-            pthread_cond_wait(&cv, &m);
+            pthread_cond_wait(&wakeupO, &m);
         O++;
-
-        if (H == 2 && O == 1)
-            pthread_cond_broadcast(&cv);
-        else
-            while (H < 2 || O < 1)
-                pthread_cond_wait(&cv, &m);
-
-        releaseOxygen();
-        released++;
-
-        if (released == 3) {
-            H = 0;
-            O = 0;
-            released = 0;
-            pthread_cond_broadcast(&cv);
+    
+        if (H == 2 && O == 1) {     
+            pthread_cond_broadcast(&wakeupH);
+            // pthread_cond_signal(&wakeupH);
+        } else {
+            while (H < 2 || O < 1) 
+                pthread_cond_wait(&wakeupO, &m);
         }
+        
+        releaseOxygen();
+        releaseO++;
+        
+        if (releaseH == 2 && releaseO == 1) {
+            H = 0, O = 0;
+            releaseH = 0, releaseO = 0;
+            pthread_cond_broadcast(&wakeupH);
+            pthread_cond_broadcast(&wakeupO);
+        }
+
         pthread_mutex_unlock(&m);
     }
 };
