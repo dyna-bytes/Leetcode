e360d6f8107731a3dea5553716c35c867cfc5699
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-01-20 22:41:14 +0900

    Time: 0 ms (100.00%) | Memory: 25.1 MB (55.03%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -1,7 +1,20 @@
+#define debug(x) cout << #x << " is " << x << endl; 
+
 class Solution {
     int min_x(int n) {
-        for (int x = 0; x < n; x++) {
-            if ((x | (x + 1)) == n) return x;
+        for (int i = 0; i < 32; i++) {
+            int x = 1 << i;
+            if (n & x) {
+                for (int j = i + 1; j < 32; j++) {
+                    int y = 1 << j;
+                    if ((n & y) == 0) {
+                        --j;
+                        int ret = n ^ (1 << j);
+                        if (ret == 0) return -1;
+                        return ret;
+                    }
+                }
+            }
         }
         return -1;
     }
