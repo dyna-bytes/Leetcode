22aa2189a30c67c169b244976c8d301999da6348
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-02-05 21:00:25 +0900

    Time: 4 ms (79.67%) | Memory: 25.9 MB (8.08%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -6,7 +6,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             int step = (i + nums[i]) % n;
-            if (step < 0) step = (step + n) % n;
+            step = (step + n) % n;
             res[i] = nums[step];
         }
         return res;
