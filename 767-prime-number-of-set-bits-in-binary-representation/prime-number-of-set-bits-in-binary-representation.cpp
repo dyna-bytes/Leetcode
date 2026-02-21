a816bebfa8f258703183c605c5e1075b6356c111
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-02-21 11:49:40 +0900

    Time: 378 ms (5.00%) | Memory: 303.1 MB (5.09%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<int> dp;
+    bool is_prime(int n) {
+        int& ret = dp[n];
+        if (ret != -1) return ret;
+
+        if (n < 2) return ret = false;
+        if (n == 2 || n == 3) return ret = true;
+        if (n % 2 == 0) return ret = false;
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return ret = false;
+        }
+        return ret = true;
+    }
+public:
+    int countPrimeSetBits(int left, int right) {
+        dp.resize(1e6 + 1, -1);
+
+        int ret = 0;
+        for (int i = left; i <= right; ++i) {
+            bitset<32> b(i);
+            int cnt = b.count();
+            if (is_prime(cnt)) ret++;
+        }
+        return ret;
+    }
+};
