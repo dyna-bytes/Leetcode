86626708cefa62f6596f7d5b354043ac16b04fb7
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-02-24 23:02:49 +0900

    Time: 14 ms (35.80%) | Memory: 8.7 MB (98.44%) - LeetSync

--- 1203-print-in-order/print-in-order.c
+++ 1203-print-in-order/print-in-order.c
@@ -1,10 +1,11 @@
 #define debug(x) \
-    printf("[%s](%d) %d\n", __func__, __LINE__, x)
+    printf("[%s](%d) x is %d\n", __func__, __LINE__, x)
 
 typedef struct {
     // User defined data may be declared here.
-    sem_t first_done;
-    sem_t second_done;
+    int cnt;
+    pthread_mutex_t m;
+    pthread_cond_t cv;
 } Foo;
 
 // Function Declaration, do not remove
@@ -16,32 +17,50 @@ Foo* fooCreate() {
     Foo* obj = (Foo*) malloc(sizeof(Foo));
     
     // Initialize user defined data here.
-    sem_init(&obj->first_done, 0, 0);
-    sem_init(&obj->second_done, 0, 0);
+    obj->cnt = 0;
+    pthread_mutex_init(&obj->m, 0);
+    pthread_cond_init(&obj->cv, 0);
     return obj;
 }
 
 void first(Foo* obj) {
-    
+    pthread_mutex_lock(&obj->m);
+    while (obj->cnt != 0)
+        pthread_cond_wait(&obj->cv, &obj->m);
+    pthread_mutex_unlock(&obj->m);
+
     // printFirst() outputs "first". Do not change or remove this line.
     printFirst();
-    sem_post(&obj->first_done);
+    obj->cnt = 1;
+    pthread_cond_broadcast(&obj->cv);
 }
 
 void second(Foo* obj) {
-    sem_wait(&obj->first_done);
+    pthread_mutex_lock(&obj->m);
+    while (obj->cnt != 1)
+        pthread_cond_wait(&obj->cv, &obj->m);
+    pthread_mutex_unlock(&obj->m);
+    
     // printSecond() outputs "second". Do not change or remove this line.
     printSecond();
-    sem_post(&obj->second_done);
+    obj->cnt = 2;
+    pthread_cond_broadcast(&obj->cv);
 }
 
 void third(Foo* obj) {
-    sem_wait(&obj->second_done);
+    pthread_mutex_lock(&obj->m);
+    while (obj->cnt != 2)
+        pthread_cond_wait(&obj->cv, &obj->m);
+    pthread_mutex_unlock(&obj->m);
+    
     // printThird() outputs "third". Do not change or remove this line.
     printThird();
+    obj->cnt = 0;
+    pthread_cond_broadcast(&obj->cv);
 }
 
 void fooFree(Foo* obj) {
     // User defined data may be cleaned up here.
-    
+    if (obj)
+        free(obj);
 }
