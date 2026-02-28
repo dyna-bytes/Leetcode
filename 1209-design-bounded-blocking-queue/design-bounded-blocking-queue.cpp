7031b0489e80f4119307a6bc7b2fa97af28fc194
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-02-28 19:55:25 +0900

    Time: 4 ms (59.82%) | Memory: 11.8 MB (82.86%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
@@ -1,43 +1,31 @@
 class BoundedBlockingQueue {
-    int cap;
     int eq = 0;
     int dq = 0;
-    vector<int> q;
-    pthread_mutex_t m;
-    pthread_cond_t eq_done;
-    pthread_cond_t dq_done;
+    sem_t sem_eq;
+    sem_t sem_dq;
+    int cap;
+    vector<int> queue;
 public:
     BoundedBlockingQueue(int capacity) {
         this->cap = capacity + 1;
-        q.resize(cap, 0);
-        pthread_mutex_init(&m, NULL);
-        pthread_cond_init(&eq_done, NULL);
-        pthread_cond_init(&dq_done, NULL);
+        queue.resize(cap);
+        sem_init(&sem_eq, 0, 0);
+        sem_init(&sem_dq, 0, capacity);
     }
     
     void enqueue(int element) {
-        pthread_mutex_lock(&m);
-        while (isFull())
-            pthread_cond_wait(&dq_done, &m); 
-
+        sem_wait(&sem_dq);
         eq = (eq + 1) % cap;
-        q[eq] = element;
-        pthread_cond_signal(&eq_done);       
-
-        pthread_mutex_unlock(&m);
+        queue[eq] = element;
+        sem_post(&sem_eq);
     }
     
     int dequeue() {
-        pthread_mutex_lock(&m);
-        while (isEmpty())
-            pthread_cond_wait(&eq_done, &m);
-    
+        sem_wait(&sem_eq);
         dq = (dq + 1) % cap;
-        int element = q[dq];
-        pthread_cond_signal(&dq_done);
-
-        pthread_mutex_unlock(&m);
-        return element;
+        int ret = queue[dq];
+        sem_post(&sem_dq);
+        return ret;
     }
 
     bool isFull() {
