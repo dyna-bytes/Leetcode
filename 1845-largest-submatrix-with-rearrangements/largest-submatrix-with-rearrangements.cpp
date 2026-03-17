51d189e770e966a9f39c45c3301e686a525123a5
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-03-17 21:47:07 +0900

    Time: 69 ms (18.09%) | Memory: 117 MB (5.85%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,48 +1,33 @@
 class Solution {
-    // [s, e)
-    int findMax(int s, int e, vector<int>& heights) {
-        if (s >= e) return 0;
-        else if (s + 1 == e) return heights[s];
-
-        int m = (s + e)/2;
-        int l = m, r = m;
-        int max_h = heights[m];
-        int max_area = heights[m] * 1;
-
-        while (s < l || r + 1 < e) {
-            int p = (s < l) ? heights[l - 1] : -1;
-            int q = (r + 1 < e) ? heights[r + 1] : -1;
-
-            if (p >= q) {
-                max_h = min(max_h, p);
-                l--;
-            } else {
-                max_h = min(max_h, q);
-                r++;
-            }
-
-            max_area = max(max_area, max_h * (r - l + 1));
-        }
-
-        return max({max_area, findMax(s, m, heights), 
-            findMax(m, e, heights)});
-    }
+    typedef pair<int, int> pii;
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int Y = matrix.size(), X = matrix[0].size();
-        vector<int> heights(X, 0);
-
+        vector<pii> prevHeights;
         int ret = 0;
-        for (int y = 0; y < Y; y++) {
-            for (int x = 0; x < X; x++) {
-                if (matrix[y][x]) heights[x]++;
-                else heights[x] = 0;
+
+        for (int y = 0; y < Y; ++y) {
+            vector<pii> heights;
+            vector<int> seen(X, 0);
+
+            for (auto& [height, x]: prevHeights) {
+                if (matrix[y][x]) {
+                    heights.push_back({ height + 1, x });
+                    seen[x] = true;
+                }
             }
 
-            vector<int> copy = heights;
-            sort(copy.begin(), copy.end());
-            ret = max(ret, findMax(0, X, copy));
+            for (int x = 0; x < X; ++x) {
+                if (!seen[x] && matrix[y][x]) 
+                    heights.push_back({ 1, x });
+            }
+
+            for (int i = 0; i < heights.size(); ++i)
+                ret = max(ret, heights[i].first * (i + 1));
+
+            prevHeights = heights;
         }
+
         return ret;
     }
 };
