936fb04b1f15c1549c7c5cf8f212c565a072cb2c
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-03-29 15:29:59 +0900

    Time: 74 ms (20.00%) | Memory: 49.9 MB (10.00%) - LeetSync

--- 562-longest-line-of-consecutive-one-in-matrix/longest-line-of-consecutive-one-in-matrix.cpp
+++ 562-longest-line-of-consecutive-one-in-matrix/longest-line-of-consecutive-one-in-matrix.cpp
@@ -3,20 +3,27 @@ class Solution {
     const int dx[8] = {1, 1, 0, -1};
     int Y, X;
     vector<vector<int>> mat;
+    vector<vector<vector<int>>> dp;
+
     bool inRange(int y, int x) {
         return 0 <= y && y < Y && 0 <= x && x < X;
     }
     int dfs(int y, int x, int dir) {
         if (!inRange(y, x)) return 0;
         if (mat[y][x] == 0) return 0;
+        int& ret = dp[y][x][dir];
+        if (ret != -1) return ret;
 
         int ny = y + dy[dir], nx = x + dx[dir];
-        return 1 + dfs(ny, nx, dir);
+        return ret = 1 + dfs(ny, nx, dir);
     }
 public:
     int longestLine(vector<vector<int>>& mat) {
         Y = mat.size(), X = mat[0].size();
         this->mat = mat;
+        dp.resize(Y + 1, vector<vector<int>>(X + 1, 
+            vector<int>(4, -1)));
+
         int ret = 0;
         for (int y = 0; y < Y; y++) {
             for (int x = 0; x < X; x++) {
