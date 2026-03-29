07677d68ad77ec055bbd55379a88dc07492e0559
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-03-29 15:17:47 +0900

    Time: 6 ms (74.42%) | Memory: 31.1 MB (9.30%) - LeetSync

---
+++ 379-design-phone-directory/design-phone-directory.cpp
@@ -0,0 +1,31 @@
+class PhoneDirectory {
+    set<int> st;
+public:
+    PhoneDirectory(int maxNumbers) {
+        for (int i = 0; i < maxNumbers; i++)
+            st.insert(i);
+    }
+    
+    int get() {
+        if (st.empty()) return -1;
+        int ret = *st.begin();
+        st.erase(st.begin());
+        return ret;
+    }
+    
+    bool check(int number) {
+        return st.find(number) != st.end();
+    }
+    
+    void release(int number) {
+        st.insert(number);
+    }
+};
+
+/**
+ * Your PhoneDirectory object will be instantiated and called as such:
+ * PhoneDirectory* obj = new PhoneDirectory(maxNumbers);
+ * int param_1 = obj->get();
+ * bool param_2 = obj->check(number);
+ * obj->release(number);
+ */
