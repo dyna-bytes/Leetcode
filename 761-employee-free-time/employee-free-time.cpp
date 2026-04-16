3546a954417b6f7dde27e71560d919af9c3bd299
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-04-16 20:15:09 +0900

    Time: 28 ms (15.76%) | Memory: 16.7 MB (34.67%) - LeetSync

---
+++ 761-employee-free-time/employee-free-time.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+#define debug(x) cout << #x << " is " << x << endl;
+class Solution {
+    typedef pair<int, int> pii;
+public:
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        int n = schedule.size();
+        map<int, int> events;
+        for (auto& sch: schedule) {
+            for (Interval& intr: sch) {
+                events[intr.start]++;
+                events[intr.end]--;
+            }
+        }
+
+        vector<Interval> ret;
+        int open_cnt = 0;
+        int start = -1;
+        int end = 0;
+        for (auto& [time, opens]: events) {
+            debug(time);
+            debug(opens);
+            open_cnt += opens;
+            debug(open_cnt);
+            if (open_cnt == 0 && start == -1) {
+                start = time;
+                end = -1;
+            } else if (open_cnt && end == -1) {
+                end = time;
+                ret.push_back(Interval(start, end));
+                start = -1;
+            }
+        }
+        return ret;
+    }
+};
