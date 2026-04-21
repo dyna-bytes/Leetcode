d69987b98a95b209013f6811218b0eec52101fc6
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-04-21 22:35:24 +0900

    Time: 10 ms (32.98%) | Memory: 19.5 MB (97.51%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    unordered_map<string,
-                  priority_queue<string, vector<string>, greater<string>>>
-        adj;
+    map<string, multiset<string>> adj;
+
     void dfs(const string& curr, vector<string>& ret) {
-        while (!adj[curr].empty()) {
-            string next = adj[curr].top();
-            adj[curr].pop();
+        while (adj[curr].size()) {
+            string next = *adj[curr].begin();
+            adj[curr].erase(adj[curr].begin());
+
             dfs(next, ret);
         }
-        ret.push_back(curr); 
+        ret.push_back(curr);
     }
-
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        for (auto& ticket : tickets) {
+        for (auto& ticket: tickets) {
             string& from = ticket[0];
             string& to = ticket[1];
-            adj[from].push(to);
+            adj[from].insert(to);
         }
+
         vector<string> ret;
         dfs("JFK", ret);
         reverse(ret.begin(), ret.end());
