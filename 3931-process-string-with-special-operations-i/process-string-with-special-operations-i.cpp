60f4d641c86b30beaad3ff603013823c4b02622d
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-17 06:06:41 +0900

    Time: 0 ms (100.00%) | Memory: 39.9 MB (42.97%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -5,12 +5,8 @@ public:
         for (char c: s) {
             if (c >= 'a' && c <= 'z') ret.push_back(c);
             else if (c == '*' && ret.size()) ret.pop_back();
-            else if (c == '#') {
-                int n = ret.size();
-                for (int i = 0; i < n; i++)
-                    ret.push_back(ret[i]);
-            } else if (c == '%')
-                reverse(ret.begin(), ret.end());
+            else if (c == '#') ret = ret + ret;
+            else if (c == '%') reverse(ret.begin(), ret.end());
         }
         return ret;
     }
