af62c928f34fe9967a2a00831b453c915e51b759
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-19 08:37:06 +0900

    Time: 0 ms (100.00%) | Memory: 12.1 MB (96.10%) - LeetSync

--- 1186-building-h2o/building-h2o.c
+++ 1186-building-h2o/building-h2o.c
@@ -3,9 +3,10 @@
 
 typedef struct {
     // User defined data may be declared here.
-    int h;
-    sem_t H;
-    sem_t O;
+    _Atomic(int) h;
+    pthread_mutex_t m;
+    pthread_cond_t cv_H;
+    pthread_cond_t cv_O;
 } H2O;
 
 void releaseHydrogen();
@@ -17,34 +18,39 @@ H2O* h2oCreate() {
     
     // Initialize user defined data here.
     obj->h = 0;
-    sem_init(&obj->H, 0, 2);
-    sem_init(&obj->O, 0, 0);
+    pthread_mutex_init(&obj->m, 0);
+    pthread_cond_init(&obj->cv_H, 0);
+    pthread_cond_init(&obj->cv_O, 0);
     return obj;
 }
 
 void hydrogen(H2O* obj) {
-    sem_wait(&obj->H);
+    pthread_mutex_lock(&obj->m);
+    while (obj->h == 2)
+        pthread_cond_wait(&obj->cv_H, &obj->m);
+    if (obj->h < 2) ++obj->h;
 
-    ++obj->h;
-    if (obj->h == 2){
-        sem_post(&obj->O);
-        obj->h = 0;
-    }
     // releaseHydrogen() outputs "H". Do not change or remove this line.
     releaseHydrogen();
+    pthread_cond_signal(&obj->cv_O);
+    pthread_mutex_unlock(&obj->m);
 }
 
 void oxygen(H2O* obj) {
-    sem_wait(&obj->O);
+    pthread_mutex_lock(&obj->m);
+    while (obj->h < 2)
+        pthread_cond_wait(&obj->cv_O, &obj->m);
+    obj->h = 0;
     // releaseOxygen() outputs "O". Do not change or remove this line.
     releaseOxygen();
-    sem_post(&obj->H);
-    sem_post(&obj->H);
+    pthread_cond_broadcast(&obj->cv_H);
+    pthread_mutex_unlock(&obj->m);
 }
 
 void h2oFree(H2O* obj) {
     // User defined data may be cleaned up here.
-    sem_destroy(&obj->H);
-    sem_destroy(&obj->O);
+    pthread_mutex_destroy(&obj->m);
+    pthread_cond_destroy(&obj->cv_H);
+    pthread_cond_destroy(&obj->cv_O);
     free(obj);
 }
