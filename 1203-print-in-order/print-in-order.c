16c34a53879937ae8554dbac9ad96c2df92ad33b
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-19 00:07:08 +0900

    Time: 0 ms (100.00%) | Memory: 9.5 MB (68.39%) - LeetSync

--- 1203-print-in-order/print-in-order.c
+++ 1203-print-in-order/print-in-order.c
@@ -1,8 +1,7 @@
 typedef struct {
     // User defined data may be declared here.
-    pthread_mutex_t m;
-    pthread_cond_t cv;
-    _Atomic(int) cnt;
+    sem_t sem1;
+    sem_t sem2;
 } Foo;
 
 // Function Declaration, do not remove
@@ -14,42 +13,27 @@ Foo* fooCreate() {
     Foo* obj = (Foo*) malloc(sizeof(Foo));
     
     // Initialize user defined data here.
-    pthread_mutex_init(&obj->m, 0);
-    pthread_cond_init(&obj->cv, 0);
-    obj->cnt = 0;
+    sem_init(&obj->sem1, 0, 0);
+    sem_init(&obj->sem2, 0, 0);
     return obj;
 }
 
 void first(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 0)
-        pthread_cond_wait(&obj->cv, &obj->m);
-    pthread_mutex_unlock(&obj->m);
+    
     // printFirst() outputs "first". Do not change or remove this line.
     printFirst();
-
-    obj->cnt = 1;
-    pthread_cond_broadcast(&obj->cv);
+    sem_post(&obj->sem1);
 }
 
 void second(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 1)
-        pthread_cond_wait(&obj->cv, &obj->m);
-    pthread_mutex_unlock(&obj->m);
+    sem_wait(&obj->sem1);
     // printSecond() outputs "second". Do not change or remove this line.
     printSecond();
-
-    obj->cnt = 2;
-    pthread_cond_broadcast(&obj->cv);
+    sem_post(&obj->sem2);
 }
 
 void third(Foo* obj) {
-    pthread_mutex_lock(&obj->m);
-    while (obj->cnt != 2)
-        pthread_cond_wait(&obj->cv, &obj->m);
-    pthread_mutex_unlock(&obj->m);
-
+    sem_wait(&obj->sem2);
     // printThird() outputs "third". Do not change or remove this line.
     printThird();
 }
