559540b9df7c134dcadead9ee02a37d7b83cdf0c
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-22 17:25:40 +0900

    Time: 25 ms (73.17%) | Memory: 23.9 MB (14.63%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.c
+++ 2587-design-memory-allocator/design-memory-allocator.c
@@ -1,104 +1,57 @@
 
+#define debug(x) printf("[%s](%d) %s is %d\n", __func__, __LINE__, #x, x);
+#define MAXN (1000 + 5)
 typedef struct {
-    int index;
-    int size;
-    int mID;
-} Page;
-
-typedef struct node {
-    Page page;
-    struct node* prev;
-    struct node* next;
-} Node;
-
-typedef struct {
-    Node* head;
+    int n;
+    int mem[MAXN];
 } Allocator;
 
 
 Allocator* allocatorCreate(int n) {
     Allocator* obj = malloc(sizeof(*obj));
-    Node* node = calloc(1, sizeof(*node));
-    node->page.index = 0;
-    node->page.size = n;
-    node->page.mID = -1;
-
-    obj->head = node;
+    obj->n = n;
+    memset(obj->mem, 0, sizeof(obj->mem));
     return obj;
 }
 
 int allocatorAllocate(Allocator* obj, int size, int mID) {
-    // printf("[%s](%d) %d \n", __func__, __LINE__, mID);
-    Node* head = obj->head;
+    int n = obj->n;
+    int* mem = obj->mem;
+    for (int s = 0, e; s + size <= n; ) {
+        bool find = true;
+        for (e = s; e < s + size; e++) {
+            if (mem[e]) {
+                find = false;
+                break;
+            }
+        }
 
-    for (Node* curr = head; curr; curr = curr->next) {
-        if (curr->page.size < size || curr->page.mID != -1) continue;
-
-        curr->page.mID = mID;
-        if (curr->page.size == size)
-            return curr->page.index;
-
-        Node* next = calloc(1, sizeof(*next));
-        next->page.size = curr->page.size - size;
-        next->page.index = curr->page.index + size;
-        next->page.mID = -1;
-
-        next->prev = curr;
-        next->next = curr->next;
-        if (next->next)
-            next->next->prev = next;
-
-        curr->next = next;
-        curr->page.size = size;
-        return curr->page.index;
+        if (find) {
+            for (int e = s; e < s + size; e++)
+                mem[e] = mID;
+            return s;
+        }
+        
+        s = e + 1;
     }
-
     return -1;
 }
 
 int allocatorFreeMemory(Allocator* obj, int mID) {
-    // printf("[%s](%d) %d \n", __func__, __LINE__, mID);
-    int sz = 0;
-    Node* head = obj->head;
-    for (Node* curr = head; curr; curr = curr->next) {
-        // printf("mID[%d] size[%d] index[%d]\n", 
-        //     curr->page.mID, curr->page.size, curr->page.index);
-        if (curr->page.mID != mID) continue;
-
-        curr->page.mID = -1;
-        sz += curr->page.size;
-
-        Node* prev = curr->prev;
-        Node* next = curr->next;
-
-        if (prev && prev->page.mID == -1) {
-            prev->page.size += curr->page.size;
-            prev->next = next;
-
-            if (next)
-                next->prev = prev;
-
-            curr = prev;
-        }
-
-        if (next && next->page.mID == -1) {
-            curr->page.size += next->page.size;
-            curr->next = next->next;
-            if (next->next)
-                next->next->prev = curr;
+    int ret = 0;
+    int n = obj->n;
+    int* mem = obj->mem;
+    for (int i = 0; i < n; i++) {
+        if (mem[i] == mID) {
+            mem[i] = 0;
+            ret++;
         }
     }
-
-    return sz;
+    return ret;
 }
 
 void allocatorFree(Allocator* obj) {
-    Node* curr = obj->head;
-    for (Node* next; curr; curr = next) {
-        next = curr->next;
-        free(curr);
-    }
-    free(obj);
+    
 }
 
 /**
