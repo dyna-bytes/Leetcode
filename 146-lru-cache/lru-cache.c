30694265dc3c69043d64fe8e4abc644d9c833da7
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-24 00:01:19 +0900

    Time: 57 ms (56.95%) | Memory: 115.9 MB (36.56%) - LeetSync

--- 146-lru-cache/lru-cache.c
+++ 146-lru-cache/lru-cache.c
@@ -1,123 +1,112 @@
+#define debug(x) printf("[%s](%d) %s is %d\n", __func__, __LINE__, #x, x);
+#define MAXK 10000 + 1
 
-#define MAXKEY 10000 + 1
 typedef struct {
     int key;
-    int val;
+    int value;
 } Page;
 
-typedef struct Node_t {
+typedef struct node_t {
     Page page;
-    struct Node_t* prev;
-    struct Node_t* next;
+    struct node_t* prev;
+    struct node_t* next;
 } Node;
 
 typedef struct {
-    Node** lut; // { key: node* }
-    int cap;
-    Node* head;    
+    Node** lut;
+    Node* head;
     Node* tail;
-    int list_size;    
+    int cap;
+    int sz;
 } LRUCache;
-
-void removeNode(Node* node) {
-    Node* prev = node->prev;
-    Node* next = node->next;
-    prev->next = next;
-    next->prev = prev;
-} 
-
-void insertTail(Node* node, Node* tail) {
-    Node* prev = tail->prev;
-    prev->next = node;
-    node->prev = prev;
-
-    node->next = tail;
-    tail->prev = node;
-}
 
 LRUCache* lRUCacheCreate(int capacity) {
     LRUCache* obj = malloc(sizeof(*obj));
 
-    Node** lut = (Node**)calloc(MAXKEY, sizeof(Node*));
+    Node** lut = calloc(MAXK, sizeof(Node*));
     obj->lut = lut;
-    obj->cap = capacity;
 
-    Node* head = malloc(sizeof(*head));
-    obj->head = head;
+    Node* head = calloc(1, sizeof(*head));
+    Node* tail = calloc(1, sizeof(*tail));
 
-    Node* tail = malloc(sizeof(*tail));
-    obj->tail = tail;
-    
-    head->prev = tail->next = NULL;
     head->next = tail;
     tail->prev = head;
 
-    obj->list_size = 0;
+    obj->head = head;
+    obj->tail = tail;
+
+    obj->cap = capacity;
+    obj->sz = 0;
     return obj;
 }
 
+void removeNode(Node* node) {
+    // debug(node->page.key);
+    // debug(node->page.value);
+    Node* prev = node->prev;
+    Node* next = node->next;
+    prev->next = next;
+    next->prev = prev;
+}
+
+void insertBack(Node* node, Node* tail) {
+    // debug(node->page.key);
+    // debug(node->page.value);
+    Node* prev = tail->prev;
+    prev->next = node;
+    node->prev = prev;
+
+    tail->prev = node;
+    node->next = tail;
+}
+
 int lRUCacheGet(LRUCache* obj, int key) {
+
     Node** lut = obj->lut;
     if (lut[key] == NULL) return -1;
-
     Node* node = lut[key];
-    removeNode(node);
-    insertTail(node, obj->tail);
 
-    Page* page = &node->page;
-    return page->val;
+    removeNode(node);
+    insertBack(node, obj->tail);
+
+    return node->page.value;
 }
 
 void lRUCachePut(LRUCache* obj, int key, int value) {
     Node** lut = obj->lut;
+    int* sz = &obj->sz;
+    int cap = obj->cap;
+
     if (lut[key]) {
         Node* node = lut[key];
-        removeNode(node);
-        insertTail(node, obj->tail);
-
-        Page* page = &node->page;
-        page->val = value;
+        node->page.value = value;
+        lRUCacheGet(obj, key);
         return;
     }
 
-    int* size = &obj->list_size;
-    if (++(*size) > obj->cap) {
-        Node* head = obj->head;
-        Node* node = head->next;
-        removeNode(node);
-
-        Page* page = &node->page;
-        lut[page->key] = NULL;
-
-        free(node);
-        --(*size);
+    if (*sz >= cap) {
+        Node* victim = obj->head->next;
+        lut[victim->page.key] = NULL;
+        removeNode(victim);
+        free(victim);
+        (*sz)--;
     }
 
     Node* node = malloc(sizeof(*node));
-    node->prev = node->next = NULL;
-    Page* page = &node->page;
-    page->key = key;
-    page->val = value;
-
+    node->page.key = key;
+    node->page.value = value;
+    insertBack(node, obj->tail);
     lut[key] = node;
-    insertTail(node, obj->tail);
+    (*sz)++;
 }
 
 void lRUCacheFree(LRUCache* obj) {
-    if (obj->lut)
-        free(obj->lut);
-
     Node* head = obj->head;
-    Node* tail = obj->tail;
-    if (head && tail && obj->list_size) {
-        for (Node* curr = head->next, *next; curr && (curr != tail); curr = next) {
-            next = curr->next;
-            free(curr);
-        }
-
-        free(head);
-        free(tail);
+    for (Node* curr = head, *next; curr; curr = next) {
+        next = curr->next;
+        free(curr);
     }
+    free(obj);
 }
 
 /**
