7cec56aec6750f079ce13d19daed3e541fdb800a
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-06-29 05:36:16 +0900

    Time: 11 ms (24.66%) | Memory: 13.4 MB (39.73%) - LeetSync

--- 1186-building-h2o/building-h2o.c
+++ 1186-building-h2o/building-h2o.c
@@ -3,10 +3,10 @@
 
 typedef struct {
     // User defined data may be declared here.
-    _Atomic(int) h;
     pthread_mutex_t m;
-    pthread_cond_t cv_H;
-    pthread_cond_t cv_O;
+    pthread_cond_t cv_hy;
+    pthread_cond_t cv_ox;
+    _Atomic(int) h;
 } H2O;
 
 void releaseHydrogen();
@@ -17,40 +17,38 @@ H2O* h2oCreate() {
     H2O* obj = (H2O*) malloc(sizeof(H2O));
     
     // Initialize user defined data here.
+    pthread_mutex_init(&obj->m, NULL);
+    pthread_cond_init(&obj->cv_hy, NULL);
+    pthread_cond_init(&obj->cv_ox, NULL);
     obj->h = 0;
-    pthread_mutex_init(&obj->m, 0);
-    pthread_cond_init(&obj->cv_H, 0);
-    pthread_cond_init(&obj->cv_O, 0);
     return obj;
 }
 
 void hydrogen(H2O* obj) {
     pthread_mutex_lock(&obj->m);
     while (obj->h == 2)
-        pthread_cond_wait(&obj->cv_H, &obj->m);
-    if (obj->h < 2) ++obj->h;
+        pthread_cond_wait(&obj->cv_hy, &obj->m);
 
+    if (++obj->h == 2)
+        pthread_cond_signal(&obj->cv_ox);
     // releaseHydrogen() outputs "H". Do not change or remove this line.
     releaseHydrogen();
-    pthread_cond_signal(&obj->cv_O);
     pthread_mutex_unlock(&obj->m);
 }
 
 void oxygen(H2O* obj) {
     pthread_mutex_lock(&obj->m);
     while (obj->h < 2)
-        pthread_cond_wait(&obj->cv_O, &obj->m);
+        pthread_cond_wait(&obj->cv_ox, &obj->m);
+
     obj->h = 0;
+    pthread_cond_broadcast(&obj->cv_hy);
     // releaseOxygen() outputs "O". Do not change or remove this line.
     releaseOxygen();
-    pthread_cond_broadcast(&obj->cv_H);
     pthread_mutex_unlock(&obj->m);
 }
 
 void h2oFree(H2O* obj) {
     // User defined data may be cleaned up here.
-    pthread_mutex_destroy(&obj->m);
-    pthread_cond_destroy(&obj->cv_H);
-    pthread_cond_destroy(&obj->cv_O);
-    free(obj);
+    
 }
