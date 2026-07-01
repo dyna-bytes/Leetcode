b6f83fb781ac6492e095b42a1cf75b41c284e8b8
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-07-02 08:27:26 +0900

    Time: 32 ms (97.95%) | Memory: 115.6 MB (71.86%) - LeetSync

--- 146-lru-cache/lru-cache.c
+++ 146-lru-cache/lru-cache.c
@@ -1,5 +1,5 @@
 #define debug(x) printf("[%s](%d) %s is %d\n", __func__, __LINE__, #x, x);
-#define MAXK 10000 + 1
+#define MAXN (10000 + 1)
 
 typedef struct {
     int key;
@@ -7,102 +7,85 @@ typedef struct {
 } Page;
 
 typedef struct node_t {
-    Page page;
     struct node_t* prev;
     struct node_t* next;
+    Page page;
 } Node;
 
 typedef struct {
-    Node** lut;
+    Node* lut[MAXN]; // {key: node*}
     Node* head;
     Node* tail;
+
     int cap;
     int sz;
 } LRUCache;
 
-LRUCache* lRUCacheCreate(int capacity) {
-    LRUCache* obj = malloc(sizeof(*obj));
+void insertBack(Node* node, Node* tail) {
+    tail->prev->next = node;
+    node->prev = tail->prev;
 
-    Node** lut = calloc(MAXK, sizeof(Node*));
-    obj->lut = lut;
-
-    Node* head = calloc(1, sizeof(*head));
-    Node* tail = calloc(1, sizeof(*tail));
-
-    head->next = tail;
-    tail->prev = head;
-
-    obj->head = head;
-    obj->tail = tail;
-
-    obj->cap = capacity;
-    obj->sz = 0;
-    return obj;
+    node->next = tail;
+    tail->prev = node;
 }
 
 void removeNode(Node* node) {
-    // debug(node->page.key);
-    // debug(node->page.value);
     Node* prev = node->prev;
     Node* next = node->next;
     prev->next = next;
     next->prev = prev;
 }
 
-void insertBack(Node* node, Node* tail) {
-    // debug(node->page.key);
-    // debug(node->page.value);
-    Node* prev = tail->prev;
-    prev->next = node;
-    node->prev = prev;
-
-    tail->prev = node;
-    node->next = tail;
+LRUCache* lRUCacheCreate(int capacity) {
+    LRUCache* obj = calloc(1, sizeof(*obj));
+    Node* head = calloc(1, sizeof(*head));
+    Node* tail = calloc(1, sizeof(*tail));
+    head->next = tail;
+    tail->prev = head;
+    obj->head = head;
+    obj->tail = tail;
+    obj->cap = capacity;
+    obj->sz = 0;
+    return obj;
 }
 
 int lRUCacheGet(LRUCache* obj, int key) {
-
     Node** lut = obj->lut;
     if (lut[key] == NULL) return -1;
     Node* node = lut[key];
-
     removeNode(node);
     insertBack(node, obj->tail);
-
     return node->page.value;
 }
 
 void lRUCachePut(LRUCache* obj, int key, int value) {
     Node** lut = obj->lut;
-    int* sz = &obj->sz;
-    int cap = obj->cap;
-
     if (lut[key]) {
-        Node* node = lut[key];
-        node->page.value = value;
         lRUCacheGet(obj, key);
+        lut[key]->page.value = value;
         return;
     }
 
-    if (*sz >= cap) {
-        Node* victim = obj->head->next;
-        lut[victim->page.key] = NULL;
-        removeNode(victim);
-        free(victim);
-        (*sz)--;
+    int cap = obj->cap;
+    int* sz = &obj->sz;
+    Node* head = obj->head;
+    Node* tail = obj->tail;
+    if (++(*sz) > cap) {
+        Node* evict = head->next;
+        removeNode(evict);
+        lut[evict->page.key] = NULL;
+        --(*sz);
     }
 
-    Node* node = malloc(sizeof(*node));
+    Node* node = calloc(1, sizeof(*node));
     node->page.key = key;
     node->page.value = value;
-    insertBack(node, obj->tail);
     lut[key] = node;
-    (*sz)++;
+    insertBack(node, tail);
 }
 
 void lRUCacheFree(LRUCache* obj) {
-    Node* head = obj->head;
-    for (Node* curr = head, *next; curr; curr = next) {
+    for (Node* curr = obj->head, *next; curr; curr = next) {
         next = curr->next;
         free(curr);
     }
