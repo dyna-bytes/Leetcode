2f4557be009ddbbfd3bb08a37928291b34047feb
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-07-07 06:55:20 +0900

    Time: 0 ms (100.00%) | Memory: 23.5 MB (55.88%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.c
+++ 2587-design-memory-allocator/design-memory-allocator.c
@@ -1,7 +1,4 @@
 
-#define debug(x) printf("[%s](%d) %s is %d\n", __func__, __LINE__, #x, x);
-#define MAXN 1001
-
 typedef struct {
     int index;
     int size;
@@ -15,16 +12,28 @@ typedef struct node_t {
 } Node;
 
 typedef struct {
-    Node* head;
+    Node* head;    
 } Allocator;
 
+void insertNode(Node* node, Node* prev) {
+    if (prev->next) prev->next->prev = node;
+    node->next = prev->next;
+
+    prev->next = node;
+    node->prev = prev;
+}
+
+void removeNode(Node* node) {
+    Node* prev = node->prev;
+    Node* next = node->next;
+    if (prev) prev->next = next;
+    if (next) next->prev = prev;
+}
 
 Allocator* allocatorCreate(int n) {
     Allocator* obj = calloc(1, sizeof(*obj));
     Node* head = calloc(1, sizeof(*head));
-    head->page.index = 0;
     head->page.size = n;
-    head->page.mID = 0;
     obj->head = head;
     return obj;
 }
@@ -34,19 +43,16 @@ int allocatorAllocate(Allocator* obj, int size, int mID) {
         if (curr->page.mID) continue;
         if (curr->page.size < size) continue;
 
-        curr->page.mID = mID;
         if (curr->page.size > size) {
             Node* next = calloc(1, sizeof(*next));
             next->page.index = curr->page.index + size;
             next->page.size = curr->page.size - size;
             next->page.mID = 0;
-
-            if (curr->next) curr->next->prev = next;
-            next->next = curr->next;
-            curr->next = next;
-            next->prev = curr;
+            insertNode(next, curr);
+            curr->page.size = size;
         }
-        curr->page.size = size;
+
+        curr->page.mID = mID;
         return curr->page.index;
     }
     return -1;
@@ -57,24 +63,20 @@ int allocatorFreeMemory(Allocator* obj, int mID) {
     for (Node* curr = obj->head; curr; curr = curr->next) {
         if (curr->page.mID != mID) continue;
 
-        curr->page.mID = 0;
         sz += curr->page.size;
-
+        curr->page.mID = 0;
+        
         Node* prev = curr->prev;
         if (prev && prev->page.mID == 0) {
             prev->page.size += curr->page.size;
-            if (curr->next) curr->next->prev = prev;
-            prev->next = curr->next;
-            free(curr);
+            removeNode(curr);
             curr = prev;
         }
 
         Node* next = curr->next;
         if (next && next->page.mID == 0) {
             curr->page.size += next->page.size;
-            if (next->next) next->next->prev = curr;
-            curr->next = next->next;
-            free(next);
+            removeNode(next);
         }
     }
     return sz;
