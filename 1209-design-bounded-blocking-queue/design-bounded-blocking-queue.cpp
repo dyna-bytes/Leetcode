5199238a8f55893530b73afa9bbd6938bd4117cd
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-07-07 20:14:24 +0900

    Time: 13 ms (6.21%) | Memory: 11.8 MB (82.16%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
@@ -1,38 +1,29 @@
 class BoundedBlockingQueue {
-    sem_t sem_eq;
-    sem_t sem_dq;
-    int eq;
-    int dq;
     int cap;
-    vector<int> q;
+    int* q;
+    int eq, dq;
+    sem_t sem_eq, sem_dq;
 public:
     BoundedBlockingQueue(int capacity) {
+        this->cap = capacity + 1;
+        q = (int*)calloc(cap, sizeof(int));
         sem_init(&sem_eq, 0, capacity);
         sem_init(&sem_dq, 0, 0);
-        this->cap = capacity + 1;
-        q.assign(cap, 0);
-        eq = dq = 0;
     }
     
     void enqueue(int element) {
         sem_wait(&sem_eq);
-        q[eq = (eq + 1) % cap] = element;
+        q[eq] = element;
+        eq = (eq + 1) % cap; 
         sem_post(&sem_dq);
     }
     
     int dequeue() {
         sem_wait(&sem_dq);
-        int ret = q[dq = (dq + 1) % cap];
+        int ret = q[dq];
+        dq = (dq + 1) % cap;
         sem_post(&sem_eq);
         return ret;
-    }
-
-    bool isFull() {
-        return size() == cap - 1;
-    }
-
-    bool isEmpty() {
-        return eq == dq;
     }
     
     int size() {
