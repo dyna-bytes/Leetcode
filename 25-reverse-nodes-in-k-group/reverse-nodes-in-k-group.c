21de96bcf2850fa5d212b96e886569d4c1a89d77
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-08-12 07:21:31 +0900

    Time: 0 ms (100.00%) | Memory: 12.3 MB (44.92%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.c
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.c
@@ -5,9 +5,10 @@
  *     struct ListNode *next;
  * };
  */
+
 struct ListNode* reverse(struct ListNode* head, int k) {
-    struct ListNode* prev = NULL;
-    for (struct ListNode* curr = head, *next; k; k--, curr = next) {
+    struct ListNode* prev = NULL, *next = NULL;
+    for (struct ListNode* curr = head; k > 0; k--, curr = next) {
         next = curr->next;
         curr->next = prev;
         prev = curr;
@@ -15,17 +16,17 @@ struct ListNode* reverse(struct ListNode* head, int k) {
 
     return prev;
 }
-
 struct ListNode* reverseKGroup(struct ListNode* head, int k) {
     int cnt = 0;
-    struct ListNode* ret = NULL, *curr;
-    for (curr = head; cnt < k && curr; curr = curr->next) 
-        cnt++;
-    
+    struct ListNode* curr = head;
+    for (; curr && cnt < k; cnt++) 
+        curr = curr->next;
+
     if (cnt == k) {
         struct ListNode* rev_head = reverse(head, k);
         head->next = reverseKGroup(curr, k);
         return rev_head;
     }
+
     return head;
 }
