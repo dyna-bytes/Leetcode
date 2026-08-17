e8e4c32b740a75c2d977a75afee315517d81f665
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-08-17 18:55:09 +0900

    Time: 15 ms (74.51%) | Memory: 25.4 MB (5.88%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.c
+++ 2587-design-memory-allocator/design-memory-allocator.c
@@ -1,14 +1,14 @@
 
 #define MAXN 1001
-typedef struct {
-    int size;
+typedef struct page_t {
     int index;
+    int size;
     int mID;
 } Page;
 
 typedef struct node_t {
-    struct node_t* prev;
-    struct node_t* next;
+    struct node_t* phys_prev;
+    struct node_t* phys_next;
     struct node_t* list_prev;
     struct node_t* list_next;
     Page page;
@@ -16,38 +16,38 @@ typedef struct node_t {
 
 typedef struct {
     Node* head;
-    Node* free_lists[MAXN]; // { size: node list }
-    Node* alloc_lists[MAXN]; // { mID: node list }
+    Node* free_lists[MAXN];
+    Node* alloc_lists[MAXN];
 } Allocator;
 
-void insertToList(Node** head, Node* node) {
+void add_list(Node** head, Node* node) {
+    if (*head) (*head)->list_prev = node;
     node->list_next = (*head);
     node->list_prev = NULL;
-    if (*head) (*head)->list_prev = node;
     (*head) = node;
 }
 
-void removeFromList(Node** head, Node* node) {
+void del_list(Node** head, Node* node) {
     if (node->list_prev) node->list_prev->list_next = node->list_next;
-    else (*head) = node->list_prev;
+    else (*head) = node->list_next;
 
     if (node->list_next) node->list_next->list_prev = node->list_prev;
     node->list_prev = node->list_next = NULL;
 }
 
-void insertNode(Node* node, Node* prev) {
-    if (prev->next) prev->next->prev = node;
-    node->next = prev->next;
-    node->prev = prev;
-    prev->next = node;
+void add_phys(Node* node, Node* phys_prev) {
+    if (phys_prev->phys_next) phys_prev->phys_next->phys_prev = node;
+    node->phys_next = phys_prev->phys_next;
+    phys_prev->phys_next = node;
+    node->phys_prev = phys_prev;
 }
 
-void removeNode(Node* node) {
-    Node* prev = node->prev;
-    Node* next = node->next;
-    if (prev) prev->next = next;
-    if (next) next->prev = prev;
-    node->prev = node->next = NULL;
+void del_phys(Node* node) {
+    Node* phys_prev = node->phys_prev;
+    Node* phys_next = node->phys_next;
+    if (phys_prev) phys_prev->phys_next = phys_next;
+    if (phys_next) phys_next->phys_prev = phys_prev;
+    node->phys_next = node->phys_prev = NULL;
 }
 
 Allocator* allocatorCreate(int n) {
@@ -55,34 +55,32 @@ Allocator* allocatorCreate(int n) {
     Node* head = calloc(1, sizeof(*head));
     head->page.size = n;
     obj->head = head;
-    insertToList(&obj->free_lists[n], head);
+    add_list(&obj->free_lists[n], head);
     return obj;
 }
 
 int allocatorAllocate(Allocator* obj, int size, int mID) {
     Node* best = NULL;
     for (int sz = size; sz < MAXN; ++sz) {
-        for (Node* curr = obj->free_lists[sz]; curr; curr = curr->list_next) {
-            if (!best || best->page.index > curr->page.index)
+        for (Node* curr = obj->free_lists[sz]; curr; curr = curr->list_next) 
+            if (!best || best->page.index > curr->page.index) 
                 best = curr;
-        }
     }
 
     if (!best) return -1;
-    removeFromList(&obj->free_lists[best->page.size], best);
+    del_list(&obj->free_lists[best->page.size], best);
 
     if (best->page.size > size) {
         Node* split = calloc(1, sizeof(*split));
         split->page.index = best->page.index + size;
         split->page.size = best->page.size - size;
-        split->page.mID = 0;
-        insertToList(&obj->free_lists[split->page.size], split);
-        insertNode(split, best);
+        add_phys(split, best);
+        add_list(&obj->free_lists[split->page.size], split);
         best->page.size = size;
     }
 
     best->page.mID = mID;
-    insertToList(&obj->alloc_lists[mID], best);
+    add_list(&obj->alloc_lists[mID], best);
     return best->page.index;
 }
 
@@ -90,34 +88,34 @@ int allocatorFreeMemory(Allocator* obj, int mID) {
     int sz = 0;
     for (Node* curr = obj->alloc_lists[mID], *list_next; curr; curr = list_next) {
         list_next = curr->list_next;
-        removeFromList(&obj->alloc_lists[mID], curr);
+        del_list(&obj->alloc_lists[mID], curr);
         sz += curr->page.size;
         curr->page.mID = 0;
 
-        Node* prev = curr->prev;
-        if (prev && prev->page.mID == 0) {
-            removeFromList(&obj->free_lists[prev->page.size], prev);
-            prev->page.size += curr->page.size;
-            removeNode(curr);
+        Node* phys_prev = curr->phys_prev;
+        if (phys_prev && phys_prev->page.mID == 0) {
+            del_list(&obj->free_lists[phys_prev->page.size], phys_prev);
+            phys_prev->page.size += curr->page.size;
+            del_phys(curr);
             free(curr);
-            curr = prev;
+            curr = phys_prev;
         }
 
-        Node* next = curr->next;
-        if (next && next->page.mID == 0) {
-            removeFromList(&obj->free_lists[next->page.size], next);
-            curr->page.size += next->page.size;
-            removeNode(next);
-            free(next);
+        Node* phys_next = curr->phys_next;
+        if (phys_next && phys_next->page.mID == 0) {
+            del_list(&obj->free_lists[phys_next->page.size], phys_next);
+            curr->page.size += phys_next->page.size;
+            del_phys(phys_next);
+            free(phys_next);
         }
-        insertToList(&obj->free_lists[curr->page.size], curr);
+        add_list(&obj->free_lists[curr->page.size], curr);
     }
     return sz;
 }
 
 void allocatorFree(Allocator* obj) {
-    for (Node* curr = obj->head, *next; curr; curr = next) {
-        next = curr->next;
+    for (Node* curr = obj->head, *phys_next; curr; curr = phys_next) {
+        phys_next = curr->phys_next;
         free(curr);
     }
     free(obj);
