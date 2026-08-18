d2b240de55228a7f4f7f3c9e8149230063c86cf3
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-08-18 21:27:32 +0900

    Time: 4 ms (37.58%) | Memory: 14.7 MB (12.12%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.c
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.c
@@ -6,108 +6,35 @@
  * };
  */
 
-#define SWAP(t, a, b) do { \ 
-    t tmp = a; \
-    a = b; \
-    b = tmp; \
-} while (0)
-#define ROOT 1
-#define HEAP_SIZE (1 << 14)
-typedef struct {
-    int arr[HEAP_SIZE];
-    int sz;
-} Heap;
+typedef struct ListNode listNode;
+listNode* mergeTwoLists(listNode* list1, listNode* list2) {
+    if (!list1) return list2;
+    if (!list2) return list1;
 
-Heap* createHeap() {
-    Heap* obj = calloc(1, sizeof(*obj));
-    return obj;
-}
-
-void heapify_up(Heap* obj, int index) {
-    int* arr = obj->arr;
-    int curr = index;
-    while (curr > ROOT) {
-        int parent = curr / 2;
-        if (arr[parent] <= arr[curr]) break;
-        SWAP(int, arr[parent], arr[curr]);
-        curr = parent;
+    if (list1->val < list2->val) {
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    } else {
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
     }
-}
-
-void heapify_down(Heap* obj, int index) {
-    int* arr = obj->arr;
-    int sz = obj->sz;
-    int curr = index;
-
-    while (curr < sz) {
-        int left = curr * 2;
-        int right = curr * 2 + 1;
-        int smallest = curr;
-
-        if (left <= sz && arr[left] < arr[smallest])
-            smallest = left;
-        if (right <= sz && arr[right] < arr[smallest])
-            smallest = right;
-        
-        if (smallest == curr) break;
-        SWAP(int, arr[smallest], arr[curr]);
-        curr = smallest;
-    }
-}
-
-void push(Heap* obj, int val) {
-    int* arr = obj->arr;
-    int* sz = &obj->sz;
-    arr[++(*sz)] = val;
-    heapify_up(obj, (*sz));
-}
-
-bool empty(Heap* obj) {
-    return obj->sz == 0;
-}
-
-int top(Heap* obj) {
-    if (empty(obj)) return INT_MIN;
-    return obj->arr[ROOT];
-}
-
-void pop(Heap* obj) {
-    if (empty(obj)) return;
-
-    int* arr = obj->arr;
-    int* sz = &obj->sz;
-    arr[ROOT] = arr[*sz];
-    --(*sz);
-    heapify_down(obj, ROOT);
+    return NULL;
 }
 
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
     if (listsSize == 0) return NULL;
     if (listsSize == 1) return lists[0];
 
-    Heap* heap = createHeap();
-    struct ListNode ret;
-    struct ListNode* curr = &ret;
-    bool pushed;
-    do {
-        pushed = false;
-        for (int i = 0; i < listsSize; ++i) {
-            struct ListNode* head = lists[i];
-            if (!head) continue;
-            push(heap, head->val);
-            lists[i] = head->next;
-            pushed = true;
-        } 
+    while (listsSize > 1) {
+        int newSize = 0;
 
-        int val = top(heap);
-        pop(heap);
-        if (val == INT_MIN) continue;
-
-        struct ListNode* node = calloc(1, sizeof(*node));
-        node->val = val;
-        curr->next = node;
-        curr = node;
-    } while (pushed || !empty(heap));
-    free(heap);
-    return ret.next;
+        for (int i = 0; i < listsSize; i += 2) {
+            if (i + 1 < listsSize)
+                lists[newSize++] = mergeTwoLists(lists[i], lists[i + 1]);
+            else
+                lists[newSize++] = lists[i];
+        }
+        listsSize = newSize;
+    }
+    return lists[0];
 }
