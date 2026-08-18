31babd402a4c1cf224e570a15d9888186c4c4ee2
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-08-19 08:46:56 +0900

    Time: 0 ms (100.00%) | Memory: 12.2 MB (45.32%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.c
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.c
@@ -6,27 +6,29 @@
  * };
  */
 
-struct ListNode* reverse(struct ListNode* head, int k) {
-    struct ListNode* prev = NULL, *next = NULL;
-    for (struct ListNode* curr = head; k > 0; k--, curr = next) {
+typedef struct ListNode listNode;
+
+// reverse list from head to size k. return reversed head.
+listNode* reverse(listNode* head, int k) {
+    listNode* curr = head;
+    listNode* prev = NULL;
+    listNode* next = NULL;
+    for (; curr && k > 0; k--, curr = next) {
         next = curr->next;
         curr->next = prev;
         prev = curr;
     }
-
     return prev;
 }
 struct ListNode* reverseKGroup(struct ListNode* head, int k) {
-    int cnt = 0;
-    struct ListNode* curr = head;
-    for (; curr && cnt < k; cnt++) 
-        curr = curr->next;
+    listNode* curr = head;
+    int i = 0;
+    for (; curr && i < k; i++, curr = curr->next);
 
-    if (cnt == k) {
-        struct ListNode* rev_head = reverse(head, k);
+    if (i == k) {
+        listNode* rev_head = reverse(head, k);
         head->next = reverseKGroup(curr, k);
         return rev_head;
     }
-
     return head;
 }
