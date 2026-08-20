d196d022c7d7f861b92380d7504e0c3fc3f283b4
Jihyuk Park <59600347+dyna-bytes@users.noreply.github.com>
2026-08-21 08:39:39 +0900

    Time: 3 ms (72.17%) | Memory: 11.7 MB (96.42%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.cpp
@@ -1,11 +1,14 @@
 class BoundedBlockingQueue {
     int cap;
     int* q;
-    int eq, dq;
-    sem_t sem_eq, sem_dq;
+    int eq;
+    int dq;
+    sem_t sem_eq;
+    sem_t sem_dq;
 public:
     BoundedBlockingQueue(int capacity) {
         this->cap = capacity + 1;
+        eq = dq = 0;
         q = (int*)calloc(cap, sizeof(int));
         sem_init(&sem_eq, 0, capacity);
         sem_init(&sem_dq, 0, 0);
@@ -14,7 +17,7 @@ public:
     void enqueue(int element) {
         sem_wait(&sem_eq);
         q[eq] = element;
-        eq = (eq + 1) % cap; 
+        eq = (eq + 1) % cap;
         sem_post(&sem_dq);
     }
     
